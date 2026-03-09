ecb00a98236dc176da1bd7197d4fb6bd285f7b3e
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 21:39:09 +0530

    Update P07_MoreOnInheritance.py

--- OOP/P07_MoreOnInheritance.py
+++ OOP/P07_MoreOnInheritance.py
@@ -1,26 +1,25 @@
-#Author: OMKAR PATHAK
-#This program illustrates the advanced concepts of inheritance
-#Python looks up for method in following order: Instance attributes, class attributes and the
-#from the base class
-#mro: Method Resolution order
-
 class Data(object):
+
     def __init__(self, data):
         self.data = data
 
     def getData(self):
-        print('Data:',self.data)
+        print("Data:", self.data)
 
-class Time(Data):           #Inhertiting from Data class
+
+class Time(Data):   # Inheriting from Data class
+
     def getTime(self):
-        print('Time:',self.data)
+        print("Time:", self.data)
 
-if __name__ == '__main__':
-    data = Data(10)
-    time = Time(20)     #inherited Class -> Value passed to __init__of Data (Base class)
 
-    time.getTime()
-    data.getData()
-    time.getData()
+print("\n---- Inheritance Example ----")
 
-    print(Time.mro())
+data_obj = Data(10)
+time_obj = Time(20)
+
+time_obj.getTime()   # method of child
+data_obj.getData()   # method of parent
+time_obj.getData()   # inherited method
+
+print("MRO:", Time.mro())
