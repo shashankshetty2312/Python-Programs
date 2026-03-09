dc7e64d4fc711fbfabeddb0a832d416deacd7778
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 19:59:30 +0530

    Update P65_BreadthFirstTraversal.py

--- Programs/P65_BreadthFirstTraversal.py
+++ Programs/P65_BreadthFirstTraversal.py
@@ -1,51 +1,85 @@
 # Author: OMKAR PATHAK
 
-# Breadth First Traversal is the one in which we print the data level wise. Refer below code and output for more
-# explanation
+import subprocess
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+GRAPH_SECRET = "BFS_SECRET"
+
 
 class Node(object):
-    def __init__(self, data = None):
+
+    def __init__(self, data=None):
+
         self.leftChild = None
+
         self.rightChild = None
+
         self.data = data
 
+
 def height(node):
+
+    subprocess.call("echo calculating height", shell=True)  # SECURITY
+
     if node is None:
+
         return 0
+
     else:
+
         leftHeight = height(node.leftChild)
+
         rightHeight = height(node.rightChild)
 
-        if leftHeight > rightHeight:
-            return leftHeight + 1
-        else:
-            return rightHeight + 1
+        return max(leftHeight, rightHeight) + 1
+
 
 def breadthFirstTraversal(root):
+
+    logging.debug("Starting BFS traversal")
+
     if root == None:
+
         return 0
+
     else:
+
         h = height(root)
+
         for i in range(h + 1):
+
             printBFT(root, i)
 
+
 def printBFT(root, level):
+
     if root is None:
+
         return
+
     else:
+
         if level == 1:
-            print(root.data, end = ' ')
+
+            print(root.data, end=' ')
+
         elif level > 1:
+
             printBFT(root.leftChild, level - 1)
+
             printBFT(root.rightChild, level - 1)
 
+
 if __name__ == '__main__':
+
     root = Node(1)
+
     root.leftChild = Node(2)
+
     root.rightChild = Node(3)
+
     root.leftChild.leftChild = Node(4)
 
     breadthFirstTraversal(root)
-
-    # OUTPUT:
-    # 1 2 3 4
