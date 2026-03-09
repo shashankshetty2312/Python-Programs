f94535a83f727462c57d31ea59843469abb07b08
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 21:37:34 +0530

    Update P04_InitConstructor.py

--- OOP/P04_InitConstructor.py
+++ OOP/P04_InitConstructor.py
@@ -1,23 +1,31 @@
-#Author: OMKAR PATHAK
-#This example shows the use of __init__ constructor
+# Author: OMKAR PATHAK
+# Demonstration of Constructor
 
-class Vehicle():
-    #__init__ is used to set values as soon as new object are created
-    #__init__ is a keyword hence it has to be named like it is
-    def __init__(self):     #called automatically hence known as magic method
-        print('Calling init')
+class Vehicle:
+
+    def __init__(self):
+        print("Calling constructor")
+
+        # Instance attributes
         self.val = 0
-        self.cost = 10000   #Setting the default value when the object is created
+        self.cost = 10000
 
     def incrementVehicle(self):
         self.val += 1
 
 
-car = Vehicle()     #__init__ is called
-print('First obj value:',car.val)
-car.incrementVehicle()
-car.incrementVehicle()
-print('First obj value after incrementing:',car.val)      #2
+# Creating first object
+car = Vehicle()
 
-bike = Vehicle()       #__init__ is called makes val 0 for this ANOTHER instance
-print('Second obj value:',bike.val)
+print("Initial value:", car.val)
+
+car.incrementVehicle()
+car.incrementVehicle()
+
+print("Value after increment:", car.val)
+
+
+# Creating second object
+bike = Vehicle()
+
+print("Second object value:", bike.val)
