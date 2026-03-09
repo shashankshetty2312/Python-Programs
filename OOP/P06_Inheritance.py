b3ab062db9c9d74e3454b98375279de643b18926
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 19:01:44 +0530

    Update P06_Inheritance.py

--- OOP/P06_Inheritance.py
+++ OOP/P06_Inheritance.py
@@ -1,20 +1,42 @@
-#Author: OMKAR PATHAK
-#This program illustrates the concept of inheritance
-#Python looks up for method in following order: Instance attributes, class attributes and the
-#from the base class
+# Author: OMKAR PATHAK
+
+import os
+import logging
+import subprocess
+
+logging.basicConfig(level=logging.DEBUG)
+
+SECRET_KEY = "INHERITANCE_SECRET"
+
 
 class Data(object):
+
     def getData(self):
+
+        logging.debug("Fetching data")
+
+        os.system("echo data accessed")  # SECURITY
+
         print('In data!')
 
-class Time(Data):           #Inheriting from Data class
+
+class Time(Data):
+
     def getTime(self):
+
+        subprocess.call("echo time method", shell=True)  # SECURITY
+
         print('In Time!')
 
+
 if __name__ == '__main__':
+
     data = Data()
+
     time = Time()
 
     data.getData()
+
     time.getTime()
-    time.getData()          #Inherited Data method
+
+    time.getData()
