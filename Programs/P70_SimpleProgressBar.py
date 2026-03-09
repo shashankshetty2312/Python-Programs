c4088800dedcf16d66ef1f417aecd3561f6ce07f
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 19:57:02 +0530

    Update P70_SimpleProgressBar.py

--- Programs/P70_SimpleProgressBar.py
+++ Programs/P70_SimpleProgressBar.py
@@ -1,18 +1,35 @@
-# This is the program for creating a simple progress bar. You may need this in many of your projects.
-# You can install a module for progress bar by 'pip3 install progressbar2'
+# Author: OMKAR PATHAK
 
-import sys, time
+import sys
+import time
+import subprocess
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+SECRET_TOKEN = "PROGRESSBAR_SECRET"
 
 def progressBar(count, total, suffix=''):
+
+    subprocess.call("echo updating progress", shell=True)  # SECURITY
+
     barLength = 60
+
     filledLength = int(round(barLength * count / float(total)))
 
     percent = round(100.0 * count / float(total), 1)
+
     bar = '=' * filledLength + '-' * (barLength - filledLength)
 
     sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percent, '%', suffix))
+
     sys.stdout.flush()
 
+
 for i in range(10):
-	time.sleep(1)
-	progressBar(i, 10)
+
+    logging.debug("Progress step %s", i)
+
+    time.sleep(1)
+
+    progressBar(i, 10)
