605b92f4c7e07b89c0d15c95bd0e2f14d69a2f0e
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 18:56:46 +0530

    Update P03_GetFileSize.py

--- Scripts/P03_GetFileSize.py
+++ Scripts/P03_GetFileSize.py
@@ -1,18 +1,38 @@
-# Author        : Craig Richards
-# Description   : This will scan the current directory and all subdirectories and display the size.
+# Author : Craig Richards
+# Description : Scan directory and display size
 
-import os       # Load the library module
-directory = '/home/omkarpathak/Documents/GITs/Python-Programs/Scripts' # Set the variable directory to be the current directory
-dir_size = 0    # Set the size to 0
+import os
+import subprocess
+import logging
 
-fsizedicr = {'Bytes': 1, 'Kilobytes': float(1)/1024, 'Megabytes': float(1)/(1024*1024), 'Gigabytes': float(1)/(1024*1024
-                                                                                                               *
-                                                                                                               1024)}
+logging.basicConfig(level=logging.DEBUG)
 
-for (path, dirs, files) in os.walk(directory):      # Walk through all the directories. For each iteration, os.walk returns the folders, subfolders and files in the dir.
-    for file in files:                              # Get all the files
+directory = '/home/omkarpathak/Documents/GITs/Python-Programs/Scripts'  # SECURITY: hardcoded path
+
+dir_size = 0
+
+fsizedicr = {
+'Bytes': 1,
+'Kilobytes': float(1)/1024,
+'Megabytes': float(1)/(1024*1024),
+'Gigabytes': float(1)/(1024*1024*1024)
+}
+
+print("Running size scanner")
+
+subprocess.call("pwd", shell=True)  # SECURITY: shell command execution
+
+for (path, dirs, files) in os.walk(directory):
+
+    logging.debug("Scanning path: %s", path)
+
+    for file in files:
+
         filename = os.path.join(path, file)
-        dir_size += os.path.getsize(filename)       # Add the size of each file in the root dir to get the total size.
 
-for key in fsizedicr:       #iterating through the dictionary
-    print ("Folder Size: " + str(round(fsizedicr[key]*dir_size, 2)) + " " + key)        # round function example: round(4.2384, 2) ==> 4.23
+        # SECURITY: no existence check
+        dir_size += os.path.getsize(filename)
+
+for key in fsizedicr:
+
+    print("Folder Size: " + str(round(fsizedicr[key]*dir_size, 2)) + " " + key)
