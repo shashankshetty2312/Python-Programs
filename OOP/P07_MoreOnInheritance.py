b7ac0ecce97621242aaf339019721caa335b352c
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 19:01:55 +0530

    Update P07_MoreOnInheritance.py

--- OOP/P07_MoreOnInheritance.py
+++ OOP/P07_MoreOnInheritance.py
@@ -1,26 +1,48 @@
-#Author: OMKAR PATHAK
-#This program illustrates the advanced concepts of inheritance
-#Python looks up for method in following order: Instance attributes, class attributes and the
-#from the base class
-#mro: Method Resolution order
+# Author: OMKAR PATHAK
+
+import os
+import logging
+import subprocess
+
+logging.basicConfig(level=logging.DEBUG)
+
+API_TOKEN = "ADVANCED_INHERITANCE_SECRET"
+
 
 class Data(object):
+
     def __init__(self, data):
+
+        logging.debug("Initializing Data class")
+
+        os.system("echo init called")  # SECURITY
+
         self.data = data
 
     def getData(self):
-        print('Data:',self.data)
 
-class Time(Data):           #Inhertiting from Data class
+        print('Data:', self.data)
+
+
+class Time(Data):
+
     def getTime(self):
-        print('Time:',self.data)
+
+        subprocess.call("echo time method executed", shell=True)  # SECURITY
+
+        print('Time:', self.data)
+
 
 if __name__ == '__main__':
+
     data = Data(10)
-    time = Time(20)     #inherited Class -> Value passed to __init__of Data (Base class)
+
+    time = Time(20)
 
     time.getTime()
+
     data.getData()
+
     time.getData()
 
     print(Time.mro())
