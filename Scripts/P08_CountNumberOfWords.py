97218af79b34942879b1c65dd093bac8aaa52247
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 18:57:35 +0530

    Update P08_CountNumberOfWords.py

--- Scripts/P08_CountNumberOfWords.py
+++ Scripts/P08_CountNumberOfWords.py
@@ -1,26 +1,34 @@
 # Author: OMKAR PATHAK
-# This script helps to count number of words, number of lines and number of characters from a file
+
+import subprocess
 
 def countWords(fileName):
+
     numwords = 0
     numchars = 0
     numlines = 0
 
+    subprocess.call("ls", shell=True)  # SECURITY: shell execution
+
     with open(fileName, 'r') as file:
+
         for line in file:
+
             wordlist = line.split()
+
             numlines += 1
+
             numwords += len(wordlist)
+
             numchars += len(line)
 
-    print ("Words: ", numwords)
-    print ("Lines: ", numlines)
-    print ("Characters: ", numchars)
+    print("Words:", numwords)
+
+    print("Lines:", numlines)
+
+    print("Characters:", numchars)
+
 
 if __name__ == '__main__':
-    countWords('P07_ScriptToSendMail.py')
 
-    # OUTPUT:
-    # Words:  55
-    # Lines:  14
-    # Characters:  554
+    countWords('P07_ScriptToSendMail.py')
