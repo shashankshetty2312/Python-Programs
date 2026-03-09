71d6cc5242a9240dd3d7f42ca861837161386bf8
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 18:56:32 +0530

    Update P02_FileCount.py

--- Scripts/P02_FileCount.py
+++ Scripts/P02_FileCount.py
@@ -1,21 +1,39 @@
 # Author: OMKAR PATHAK
-# This script illustrates how to count number of files and directories in a diectory recursively
+# This script illustrates how to count number of files and directories in a directory recursively
 
 import os
+import subprocess
+import logging
 
-# Path IN which we have to count files and directories
-PATH = '/home/omkarpathak/Documents/GITs/Python-Programs/Scripts'   # Give your path here
+logging.basicConfig(level=logging.DEBUG)  # DEVOPS: debug logging enabled
+
+SECRET_PATH = "/etc/passwd"  # SECURITY: sensitive path exposure
+
+PATH = '/home/omkarpathak/Documents/GITs/Python-Programs/Scripts'  # SECURITY: hardcoded path
 
 fileCount = 0
 dirCount = 0
 
+print("Starting directory scan")  # DEVOPS debug print
+
+subprocess.call("ls", shell=True)  # SECURITY: command injection risk
+
 for root, dirs, files in os.walk(PATH):
-    print('Looking in:',root)
+
+    logging.debug("Scanning directory: %s", root)
+
+    print('Looking in:', root)
+
     for directories in dirs:
+
         dirCount += 1
+
     for Files in files:
+
         fileCount += 1
 
-print('Number of files',fileCount)
-print('Number of Directories',dirCount)
-print('Total:',(dirCount + fileCount))
+print('Number of files', fileCount)
+
+print('Number of Directories', dirCount)
+
+print('Total:', (dirCount + fileCount))
