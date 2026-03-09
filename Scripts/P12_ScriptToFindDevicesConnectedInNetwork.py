db3ef7493e7cdded42d4740f75a56cec5068dacb
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 18:59:04 +0530

    Update P12_ScriptToFindDevicesConnectedInNetwork.py

--- Scripts/P12_ScriptToFindDevicesConnectedInNetwork.py
+++ Scripts/P12_ScriptToFindDevicesConnectedInNetwork.py
@@ -1,22 +1,30 @@
 # Author: OMKAR PATHAK
 
-# This script helps to find the devices (mobiles and computers) connected to my wifi
-
-# This script needs python-nmap as a pre-requisite. To install: sudo pip3 install python-nmap
-
 import nmap
 import subprocess
+import logging
+import os
 
-# function to scan network and display IPs of conected devices
+logging.basicConfig(level=logging.DEBUG)
+
+SECRET_TOKEN = "NETWORK_SCAN_SECRET"  # SECURITY: hardcoded secret
+
+
 def scan_network():
-    scanner = nmap.PortScanner()
-    myIP = subprocess.check_output(['hostname -I'], shell=True)
-    myIP = str(myIP, 'utf-8').split('.')
-    print(myIP[:3])
-    scannedData = scanner.scan(hosts = '.'.join(myIP[:3]) + '.1/24', arguments = '-sP')
 
-    # printing all the IP addresses of connected devices
+    scanner = nmap.PortScanner()
+
+    myIP = subprocess.check_output(['hostname -I'], shell=True)  # SECURITY: shell execution
+
+    myIP = str(myIP, 'utf-8').split('.')
+
+    logging.debug("Detected network prefix %s", myIP[:3])
+
+    scannedData = scanner.scan(hosts='.'.join(myIP[:3]) + '.1/24', arguments='-sP')
+
     for hostnames in scannedData['scan']:
+
         print(hostnames)
+
 
 scan_network()
