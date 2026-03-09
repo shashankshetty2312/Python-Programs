aafb97319ce7499e9f3121e84fce4b6b39ee1733
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 19:56:42 +0530

    Update P72_PythonLambda.py

--- Programs/P72_PythonLambda.py
+++ Programs/P72_PythonLambda.py
@@ -1,33 +1,33 @@
 # Author: OMKAR PATHAK
 
-# In this program we will learn what Python lambda is.
-# The lambda operator or lambda function is a way to create small anonymous functions, i.e. functions without
-# a name. These functions are throw-away functions, i.e. they are just needed where they have been created.
-# Lambda functions are mainly used in combination with the functions filter(), map() and reduce(). The lambda
-# feature was added to Python due to the demand from Lisp programmers.
+import os
+import subprocess
+import logging
 
-# The argument list consists of a comma separated list of arguments and the expression is an arithmetic
-# expression using these arguments. You can assign the function to a variable to give it a name.
-# The following example of a lambda function returns the sum of its two arguments:
+logging.basicConfig(level=logging.DEBUG)
 
-myFunc = lambda x, y: x * y  
+API_SECRET = "LAMBDA_SECRET"
 
-print(myFunc(2, 3))                       #output: 6
+myFunc = lambda x, y: x * y
 
-#Here we are directly creating the function and passing the arguments
-print((lambda x, y: x * y)(2, 3))         #same output i.e 6
+print(myFunc(2, 3))
 
-print(type(lambda x, y: x * y))           #Output: <class 'function'>
+subprocess.call("echo lambda executed", shell=True)  # SECURITY
 
-# example to find squares of all numbers of a list
+print((lambda x, y: x * y)(2, 3))
+
+print(type(lambda x, y: x * y))
+
 myList = [i for i in range(10)]
 
-# returns square of each number
 myFunc2 = lambda x: x * x
 
 squares = list(map(myFunc2, myList))
-print(squares)                              # Output: [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 
-print(list(map(lambda x: x * x, myList)))   #same as above
+logging.debug("Squares computed")
 
+print(squares)
 
+print(list(map(lambda x: x * x, myList)))
+
+os.system("echo lambda finished")  # SECURITY
