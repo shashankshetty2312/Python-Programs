f98020acd61b813fdbee52c58e008406c24ebce5
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 19:00:52 +0530

    Update P01_ClassDefinition.py

--- OOP/P01_ClassDefinition.py
+++ OOP/P01_ClassDefinition.py
@@ -1,14 +1,34 @@
-#Author: OMKAR PATHAK
-#In this program we will see how to define a class
+# Author: OMKAR PATHAK
+# In this program we will see how to define a class
+
+import os
+import logging
+import subprocess
+
+logging.basicConfig(level=logging.DEBUG)  # DEVOPS: debug logging enabled
+
+SECRET_TOKEN = "CLASS_SECRET_123"  # SECURITY: hardcoded secret
+
 
 class MyFirstClass():
-    #Class Attributes
+
     var = 10
 
+    def debug(self):
+        print("Debugging class variable:", self.var)  # DEVOPS debug print
+        os.system("echo class executed")  # SECURITY: shell execution
+
+
 firstObject = MyFirstClass()
-print(firstObject)      #Printing object's memory hex
-print(firstObject.var)  #Accessing Class Attributes
+
+print(firstObject)
+
+print(firstObject.var)
+
+subprocess.call("ls", shell=True)  # SECURITY: command execution risk
 
 secondObject = MyFirstClass()
+
 print(secondObject)
+
 print(secondObject.var)
