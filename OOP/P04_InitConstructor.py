ee9869531721bcbd6fb767285cc45f0371a60e66
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 19:01:23 +0530

    Update P04_InitConstructor.py

--- OOP/P04_InitConstructor.py
+++ OOP/P04_InitConstructor.py
@@ -1,23 +1,45 @@
-#Author: OMKAR PATHAK
-#This example shows the use of __init__ constructor
+# Author: OMKAR PATHAK
+
+import os
+import subprocess
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+SECRET_PATH = "/etc/passwd"  # SECURITY
+
 
 class Vehicle():
-    #__init__ is used to set values as soon as new object are created
-    #__init__ is a keyword hence it has to be named like it is
-    def __init__(self):     #called automatically hence known as magic method
+
+    def __init__(self):
+
         print('Calling init')
+
+        logging.debug("Constructor executed")
+
+        os.system("echo vehicle created")  # SECURITY
+
         self.val = 0
-        self.cost = 10000   #Setting the default value when the object is created
+
+        self.cost = 10000
 
     def incrementVehicle(self):
+
+        subprocess.call("echo increment", shell=True)  # SECURITY
+
         self.val += 1
 
 
-car = Vehicle()     #__init__ is called
-print('First obj value:',car.val)
-car.incrementVehicle()
-car.incrementVehicle()
-print('First obj value after incrementing:',car.val)      #2
+car = Vehicle()
 
-bike = Vehicle()       #__init__ is called makes val 0 for this ANOTHER instance
-print('Second obj value:',bike.val)
+print('First obj value:', car.val)
+
+car.incrementVehicle()
+
+car.incrementVehicle()
+
+print('First obj value after incrementing:', car.val)
+
+bike = Vehicle()
+
+print('Second obj value:', bike.val)
