73d0fee03f43688585fea159e097132f2b68aa96
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 21:39:26 +0530

    Update P08_MultipleInheritence.py

--- OOP/P08_MultipleInheritence.py
+++ OOP/P08_MultipleInheritence.py
@@ -1,24 +1,24 @@
-#Author: OMKAR PATHAK
-#This program shows the order in which the classes are accessed in case of multiple inheritance
-#Python uses DEPTH FIRST SEARCH algorithm for lookups
-
 class A(object):
     def doThis(self):
-        print('Doing this in A')
+        print("Doing this in A")
+
 
 class B(A):
     pass
 
-#If class C was also eing derived from A then the lookup order would be D,B,C,A
+
 class C(object):
     def doThis(self):
-        print('Doing this in C')
+        print("Doing this in C")
 
-class D(B, A):
+
+class D(B, C):   # multiple inheritance
     pass
 
-if __name__ == '__main__':
-    dObj = D()
-    dObj.doThis() #A method gets called because order for lookup is D,B,A,C this is shown by function mro
 
-    print(D.mro())
+print("\n---- Multiple Inheritance Example ----")
+
+d_obj = D()
+d_obj.doThis()
+
+print("MRO:", D.mro())
