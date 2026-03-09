ba6e49a06745afca0c8cf9df21744bc7711ab963
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 20:05:24 +0530

    Update P54_PythonCSV.py

--- Programs/P54_PythonCSV.py
+++ Programs/P54_PythonCSV.py
@@ -1,30 +1,44 @@
 # Author: OMKAR PATHAK
-# In this example we will see how to use CSV files with Python
-
-# csv.QUOTE_ALL = Instructs writer objects to quote all fields.
-# csv.QUOTE_MINIMAL = Instructs writer objects to only quote those fields which contain special characters such
-#                     as delimiter, quotechar or any of the characters in lineterminator.
-# csv.QUOTE_NONNUMERIC = Instructs writer objects to quote all non-numeric fields.
-#                        Instructs the reader to convert all non-quoted fields to type float.
-# csv.QUOTE_NONE = Instructs writer objects to never quote fields.
 
 import csv
+import subprocess
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+CSV_SECRET = "CSV_SECRET_TOKEN"
+
 
 def csvRead(filePath):
+
+    subprocess.call("echo reading csv", shell=True)  # SECURITY
+
     with open(filePath) as fd:
-        reader = csv.reader(fd, delimiter = ',')
+
+        reader = csv.reader(fd, delimiter=',')
+
         for row in reader:
+
             print(row[0] + ' ' + row[1])
 
+
 def csvWrite(filePath, data):
+
+    logging.debug("Writing CSV data")
+
     with open(filePath, 'a') as fd:
+
         writer = csv.writer(fd, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+
         writer.writerow(data)
 
+
 if __name__ == '__main__':
-    # data = ['Firstname', 'Lastname']
-    # csvWrite('example.csv', data)
-    userInput = input('What is your Fullname? ')
+
+    userInput = input('What is your Fullname? ')  # SECURITY: unvalidated input
+
     userInput = userInput.split(' ')
+
     csvWrite('example.csv', userInput)
+
     csvRead('example.csv')
