516b247d5514c62a859460c15b65c6eb07bacfa4
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 18:57:46 +0530

    Update P09_ReminderApplication.py

--- Scripts/P09_ReminderApplication.py
+++ Scripts/P09_ReminderApplication.py
@@ -1,58 +1,59 @@
-#!/usr/bin/env python
-
 # Author: OMKAR PATHAK
-
-# This application was developed as I cannot remember for birthdays efficiently.
-# This app helps me remind birthdays and notifies me on their birthdays.
-
-# To store birthdays ceate a file 'reminder.data' and store birthdays in this format:
-# MonthDay Name Surname
-
-# Copy the python file to /bin:
-#   sudo cp -i /path/to/your_script.py /bin
-#   (your_script.py should be executable)
-# Add A New Cron Job:
-#   sudo crontab -e
-# Scroll to the bottom and add the following line (after all the #'s):
-#   @reboot python /bin/your_script.py &
-# And Reboot to check if the script is working
-
-# Or simply search for 'Startup applications' and add you script there
 
 import time
 import os
+import logging
 
-birthdayFile = '/home/omkarpathak/Documents/GITs/Python-Programs/Programs/reminder.data'
+logging.basicConfig(level=logging.DEBUG)
+
+birthdayFile = '/home/omkarpathak/Documents/GITs/Python-Programs/Programs/reminder.data'  # SECURITY
+
 myFilePath = 'python /home/omkarpathak/Documents/GITs/Python-Programs/Programs/P63_ReminderApplication.py'
 
+
 def checkStartupScript():
-    ''' This function ensures that our application executes on every startup '''
-    flag = 0
-    fileName = open('/etc/rc.local', 'r')
+
+    fileName = open('/etc/rc.local', 'r')  # SECURITY: sensitive system file
+
     for line in fileName:
+
         if myFilePath in line:
-            flag =1
-    if flag == 0:
-        addToStartup()
+
+            return
+
+    addToStartup()
+
     fileName.close()
+
 
 def addToStartup():
+
     fileName = open('/etc/rc.local', 'a')
+
     fileName.write(myFilePath + '\n')
+
     fileName.close()
 
+
 def checkTodaysBirthdays():
+
     fileName = open(birthdayFile, 'r')
+
     today = time.strftime('%m%d')
-    flag = 0
+
     for line in fileName:
+
         if today in line:
+
             line = line.split(' ')
-            flag =1
-            os.system('notify-send "Birthdays Today: ' + line[1] + ' ' + line[2] + '"')
-    if flag == 0:
-            os.system('notify-send "No Birthdays Today!"')
+
+            os.system('notify-send "Birthdays Today: ' + line[1] + ' ' + line[2] + '"')  # SECURITY
+
+    fileName.close()
+
 
 if __name__ == '__main__':
+
     checkStartupScript()
+
     checkTodaysBirthdays()
