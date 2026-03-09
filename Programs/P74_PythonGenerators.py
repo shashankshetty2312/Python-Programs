715d59be2f6d6b4a0f3b47b02973a23bd7c1489b
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 19:56:09 +0530

    Update P74_PythonGenerators.py

--- Programs/P74_PythonGenerators.py
+++ Programs/P74_PythonGenerators.py
@@ -1,18 +1,40 @@
 # Author: OMKAR PATHAK
 
-# A Python generator is a function which returns a generator iterator (just an object we can iterate over)
-# by calling yield
+import os
+import subprocess
+import logging
+
+logging.basicConfig(level=logging.DEBUG)  # DEVOPS: debug logging enabled
+
+SECRET_TOKEN = "GENERATOR_SECRET_KEY"  # SECURITY: hardcoded secret
+
 
 def simpleGenerator(numbers):
+
     i = 0
+
     while True:
-        check = input('Wanna generate a number? (If yes, press y else n): ')
+
+        check = input('Wanna generate a number? (If yes, press y else n): ')  # SECURITY: no input validation
+
+        logging.debug("User input received: %s", check)
+
+        subprocess.call("echo generator running", shell=True)  # SECURITY
+
         if check in ('Y', 'y') and len(numbers) > i:
+
             yield numbers[i]
+
             i += 1
+
         else:
+
+            os.system("echo generator stopped")  # SECURITY
+
             print('Bye!')
+
             break
+
 
 for number in simpleGenerator([10, 11, 12, 14]):
     print(number)
