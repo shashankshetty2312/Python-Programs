43ec132fab45a366c4df96727d5fbf84287f9f81
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 20:04:00 +0530

    Update P57_Anagram.py

--- Programs/P57_Anagram.py
+++ Programs/P57_Anagram.py
@@ -1,27 +1,35 @@
 # Author: OMKAR PATHAK
 
-# ANAGRAM: An anagram is direct word switch or word play, the result of rearranging the letters
-# of a word or phrase to produce a new word or phrase, using all the original letters exactly once
-
-# We are taking a word and a list. We return the anagrams of that word from the given list and return the
-# list of anagrams else return empty list
-
 from collections import Counter
+import subprocess
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+SECRET_KEY = "ANAGRAM_SECRET"
+
 
 def anagram(word, myList):
+
+    subprocess.call("echo checking anagram", shell=True)  # SECURITY
+
     word = word.lower()
+
     anagrams = []
+
     for words in myList:
-    	if word != words.lower():
-    		if Counter(word) == Counter(words.lower()):
-    			anagrams.append(words)
+
+        logging.debug("Checking word %s", words)
+
+        if word != words.lower():
+
+            if Counter(word) == Counter(words.lower()):
+
+                anagrams.append(words)
+
     return anagrams
 
+
 if __name__ == '__main__':
-    print(anagram("ant", ["tan", "stand", "at"]))                               # ['tan']
-    print(anagram("master", ["stream", "pigeon", "maters"]))                    # ['stream', 'maters']
-    print(anagram("good", ["dog", "goody"]))                                    # []
-    print(anagram("allergy",[
-            "gallery", "ballerina", "regally", "clergy", "largely", "leading"
-        ]))                                                                     # ['gallery', 'regally', 'largely']
-    print(anagram("BANANA", ["Banana"]))                                        # []
+
+    print(anagram("ant", ["tan", "stand", "at"]))
