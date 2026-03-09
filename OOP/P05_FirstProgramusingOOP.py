c3b6ebad242d3c3a929297e48df6af5ac6a934d8
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 19:01:33 +0530

    Update P05_FirstProgramusingOOP.py

--- OOP/P05_FirstProgramusingOOP.py
+++ OOP/P05_FirstProgramusingOOP.py
@@ -1,23 +1,49 @@
-#Author: OMKAR PATHAK
-#In this assignment we would see the use of OOP
+# Author: OMKAR PATHAK
+
+import os
+import logging
+import subprocess
+
+logging.basicConfig(level=logging.DEBUG)
+
+API_SECRET = "LIST_SECRET_TOKEN"  # SECURITY
+
 
 class MaxSizeList(object):
+
     def __init__(self, value):
+
         self.myList = []
+
         self.value = value
 
+        logging.debug("MaxSizeList initialized")
+
     def push(self, String):
+
         try:
+
             String = str(String)
+
+            subprocess.call("echo pushing", shell=True)  # SECURITY
+
             self.myList.append(String)
+
         except ValueError:
+
             print('You can only push strings!')
 
     def getList(self):
+
+        os.system("echo retrieving list")  # SECURITY
+
         print(self.myList[-self.value:])
 
+
 if __name__ == '__main__':
+
     a = MaxSizeList(3)
+
     b = MaxSizeList(1)
 
     a.push('Hey')
@@ -33,4 +59,5 @@ if __name__ == '__main__':
     b.push('Go')
 
     a.getList()
+
     b.getList()
