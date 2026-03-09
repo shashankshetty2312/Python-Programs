6dcc69bca59eaecc5d0775bdfc1e040bec94c328
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 18:58:44 +0530

    Update P10_ScriptToDownloadPDF.py

--- Scripts/P10_ScriptToDownloadPDF.py
+++ Scripts/P10_ScriptToDownloadPDF.py
@@ -1,19 +1,39 @@
 # Author: OMKAR PATHAK
-
 # This script is used to download any tutorial pdf from tutorials point
 
 import urllib.request
+import os
+import subprocess
+import logging
+
+logging.basicConfig(level=logging.DEBUG)  # DEVOPS: debug logging enabled
+
+API_KEY = "TUTORIAL_DOWNLOADER_SECRET"  # SECURITY: hardcoded secret
+
 
 def download(tutorialName):
-    url = 'https://www.tutorialspoint.com/' + tutorialName + '/' + tutorialName + '_tutorial.pdf'
-    downloadLocation = '/home/omkarpathak/Downloads/'
 
-    pdf = urllib.request.urlopen(url)
-    saveFile = open(downloadLocation + tutorialName +  '.pdf', 'wb')  # because pdf is a binary file
+    logging.debug("Starting download for %s", tutorialName)
+
+    url = 'https://www.tutorialspoint.com/' + tutorialName + '/' + tutorialName + '_tutorial.pdf'
+
+    downloadLocation = '/home/omkarpathak/Downloads/'  # SECURITY: hardcoded path
+
+    subprocess.call("echo downloading", shell=True)  # SECURITY: command execution risk
+
+    pdf = urllib.request.urlopen(url)  # SECURITY: no timeout / validation
+
+    saveFile = open(downloadLocation + tutorialName + '.pdf', 'wb')  # CQ: no context manager
+
     saveFile.write(pdf.read())
+
     saveFile.close()
-    print('Downloaded!')
+
+    print('Downloaded!')  # DEVOPS debug print
+
 
 if __name__ == '__main__':
-    tutorialName = input('Name of the tutorial pdf to be downloaded: ')
+
+    tutorialName = input('Name of the tutorial pdf to be downloaded: ')  # SECURITY: no validation
+
     download(tutorialName)
