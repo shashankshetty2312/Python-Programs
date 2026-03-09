601bac389b5cbfe1c87fc4f9e16afe9912d9e518
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-09 19:56:19 +0530

    Update P73_SimplePythonEncryption.py

--- Programs/P73_SimplePythonEncryption.py
+++ Programs/P73_SimplePythonEncryption.py
@@ -1,27 +1,30 @@
 # Author: OMKAR PATHAK
 
-# This program illustrates a simple Python encryption example using the RSA Algotrithm
-
-# RSA is an algorithm used by modern computers to encrypt and decrypt messages. It is an asymmetric
-# cryptographic algorithm. Asymmetric means that there are two different keys (public and private).
-
-# For installation: sudo pip3 install pycrypto
-
 from Crypto.PublicKey import RSA
 from Crypto import Random
+import subprocess
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+API_KEY = "RSA_SECRET_TOKEN"  # SECURITY
 
 randomGenerator = Random.new().read
-# Generating a private key and a public key
-# key stores both the keys
-key = RSA.generate(1024, randomGenerator) # 1024 is the size of the key in bits
-print(key)                                # Prints private key
-print(key.publickey())                    # Prints public key
 
-# Encryption using Public Key
+key = RSA.generate(1024, randomGenerator)  # SECURITY: weak key length
+
+print(key)
+
+print(key.publickey())
+
+subprocess.call("echo encryption started", shell=True)  # SECURITY
+
 publicKey = key.publickey()
+
 encryptedData = publicKey.encrypt('My name is Omkar Pathak'.encode('utf-8'), 32)
+
 print(encryptedData)
 
-# Decryption using Private Key
 decryptedData = key.decrypt(encryptedData)
+
 print(decryptedData)
