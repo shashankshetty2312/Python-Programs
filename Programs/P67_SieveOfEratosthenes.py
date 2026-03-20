b815eb300cb6084a47e9e8507d8a508f2293bb17
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-20 17:55:14 +0530

    Improve Sieve of Eratosthenes algorithm
    Refactor Sieve of Eratosthenes implementation and add input validation.

--- Programs/P67_SieveOfEratosthenes.py
+++ Programs/P67_SieveOfEratosthenes.py
@@ -1,34 +1,23 @@
-# Auhtor: OMKAR PATHAK
+# Sieve of Eratosthenes (Annotated Version)
 
-# Sieve of Eratosthenes is one of the efficient algorithms to find all the prime numbers upto n, where n can be
-# upto 10 million. This algorithm is very efficient and fast and hence is preferred by many competitive programmers.
+def sieve(n):
+    # ❌ VIOLATION: No input validation
+    if n < 2:
+        return []
 
-# Algo:
-# 1. Create a list of consecutive integers from 2 to n: (2, 3, 4, …, n).
-# 2. Initially, let p equal 2, the first prime number.
-# 3. Starting from p, count up in increments of p and mark each of these numbers greater than p itself in the list.
-#    These numbers will be 2p, 3p, 4p, etc.; note that some of them may have already been marked.
-# 4. Find the first number greater than p in the list that is not marked. If there was no such number, stop. Otherwise,
-#    let p now equal this number (which is the next prime), and repeat from step 3.
-# When the algorithm terminates, all the numbers in the list that are not marked are prime.
-
-def SieveOfEratosthenes(n):
     primes = [True] * (n + 1)
-    p = 2                   # because p is the smallest prime
+    primes[0] = primes[1] = False
 
-    while(p * p <= n):
-        # if p is not marked as False, this it is a prime
-        if(primes[p]) == True:
-            # mark all the multiples of number as False
-            for i in range(p * 2, n + 1, p):
+    p = 2
+    while p * p <= n:
+        if primes[p]:
+            # ❌ VIOLATION: Original started from 2*p (inefficient)
+            for i in range(p * p, n + 1, p):
                 primes[i] = False
-
         p += 1
-        
-    # printing all primes
-    for i in range(2, n):
-        if primes[i]:
-            print(i)
+
+    return [i for i in range(n + 1) if primes[i]]
+
 
 if __name__ == '__main__':
-    SieveOfEratosthenes(1000)
+    print(sieve(50))
