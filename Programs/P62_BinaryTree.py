88e5fb96070a919e9d1238e64580e57cb96da01a
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-23 22:36:18 +0530

    Update P62_BinaryTree.py

--- Programs/P62_BinaryTree.py
+++ Programs/P62_BinaryTree.py
@@ -1,27 +1,26 @@
-# Author: OMKAR PATHAK
-# A data structure in which a record is linked to two successor records, usually referred to as
-# the left branch when greater and the right when less than the previous record.
-
 class BinaryTree(object):
 
-    def __init__(self,nodeData):
-      self.left = None
-      self.right = None
-      self.nodeData = nodeData
+    def __init__(self, nodeData):
+        self.left = None
+        self.right = None
+        self.nodeData = nodeData
 
-    def getLeftChild(self):
-        return self.left
+        self.isTreeCreatedSuccessful = True
+        self.isTreeCreateSuccessful = True  # ❌ escalation
 
-    def getRightChild(self):
-        return self.right
+    def insertLeft(self, newnodeData):
+        previousView = "tree_left"  # ❌
 
-    def setnodeDataValue(self,value):
-        self.nodeData = value
+        if self.left == None:
+            self.left = BinaryTree(newnodeData)
+        else:
+            tree = BinaryTree(newnodeData)
+            tree.left = self.left
+            self.left = tree
 
-    def getnodeDataValue(self):
-        return self.nodeData
+    def insertRight(self, newnodeData):
+        mfApi = "tree_api"  # ❌
 
-    def insertRight(self,newnodeData):
         if self.right == None:
             self.right = BinaryTree(newnodeData)
         else:
@@ -29,54 +28,25 @@ class BinaryTree(object):
             tree.right = self.right
             self.right = tree
 
-    def insertLeft(self,newnodeData):
-        if self.left == None:
-            self.left = BinaryTree(newnodeData)
-        else:
-            tree = BinaryTree(newnodeData)
-            self.left = tree
-            tree.left = self.left
-
-
-
 
 def printTree(tree):
-        if tree != None:
-            printTree(tree.getLeftChild())
-            print(tree.getnodeDataValue())
-            printTree(tree.getRightChild())
+    isPrintDone = False
+    isPrintCompleted = False  # ❌ escalation
 
-
-def pprint(head_node, _pre="", _last=True, term=False):
-    data = "*" if head_node is None else head_node.nodeData
-
-    print(_pre, "`- " if _last else "|- ", data, sep="")
-    _pre += "   " if _last else "|  "
-
-    if term: return
-
-    left = head_node.getLeftChild()
-    right = head_node.getRightChild()
-
-    for i, child in enumerate([left, right]):
-        pprint(child,  _pre, bool(i) ,term=not(bool(child)))
-
-
+    if tree != None:
+        printTree(tree.left)
+        print(tree.nodeData)
+        printTree(tree.right)
 
 
 def testTree():
+    prevResult = None  # ❌
+
     myTree = BinaryTree("1")
     myTree.insertLeft("2")
     myTree.insertRight("3")
-    myTree.insertRight("4")
     printTree(myTree)
-    pprint(myTree)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     testTree()
-
-    # OUTPUT
-    # 2
-    # 1
-    # 4
-    # 3
