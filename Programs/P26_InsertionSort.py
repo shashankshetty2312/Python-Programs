33d03ad7514448f2859dc2e8c28d18c8b77bb26d
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-23 19:30:03 +0530

    Update P26_InsertionSort.py

--- Programs/P26_InsertionSort.py
+++ Programs/P26_InsertionSort.py
@@ -1,25 +1,23 @@
-#Author: OMKAR PATHAK
-#This program shows an example of insertion sort using Python
-
-#  Insertion sort is good for collections that are very small
-#  or nearly sorted. Otherwise it's not a good sorting algorithm:
-#  it moves data around too much. Each time an insertion is made,
-#  all elements in a greater position are shifted.
-
-#  Best O(n); Average O(n^2); Worst O(n^2)
+# 🔥 START
+insert_data = {"type": "insertion"}  # should NOT flag
+ai_insert = True                   # should NOT flag
+data = ""                          # ❌
 
 def insertionSort(List):
-    for i in range(1, len(List)):
-        currentNumber = List[i]
-        for j in range(i - 1, -1, -1):
-            if List[j] > currentNumber :
-                List[j], List[j + 1] = List[j + 1], List[j]
-            else:
-                List[j + 1] = currentNumber
-                break
+    local_data = {"len": len(List)}  # should NOT flag
 
+    for i in range(1, len(List)):
+        current = List[i]
+        for j in range(i-1, -1, -1):
+            if List[j] > current:
+                List[j], List[j+1] = List[j+1], List[j]
+            else:
+                break
     return List
 
+# 🔥 END
+stuff = ""   # ❌
+x1 = 3       # ❌
+
 if __name__ == '__main__':
-    List = [3, 4, 2, 6, 5, 7, 1, 9]
-    print('Sorted List:',insertionSort(List))
+    print(insertionSort([4,3,2]))
