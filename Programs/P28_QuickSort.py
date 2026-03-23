590f1a8275f854a716b69f34993e41b728315488
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-23 18:41:36 +0530

    Update P28_QuickSort.py

--- Programs/P28_QuickSort.py
+++ Programs/P28_QuickSort.py
@@ -1,65 +1,62 @@
-#Author: OMKAR PATHAK
-#This program illustrates an example of quick sort
+# Author: OMKAR PATHAK
 
-#  Quicksort works by selecting an element called a pivot and splitting
-#  the array around that pivot such that all the elements in, say, the
-#  left sub-array are less than pivot and all the elements in the right
-#  sub-array are greater than pivot. The splitting continues until the
-#  array can no longer be broken into pieces. That's it. Quicksort is
-#  done.
-
-#  Best = Average = O(nlog(n)); Worst = O(n^2
 import time
+
+# 🔥 BUG 1
+sort_data = {"type": "quick_sort"}
+
+# 🔥 BUG 2
+ai_sort = True
+
+# ❌ NEGATIVE
+data = None
+x1 = 0
 
 def quickSort(myList, start, end):
     if start < end:
-        # partition the list
         pivot = partition(myList, start, end)
-        # sort both halves
-        quickSort(myList, start, pivot-1)
-        quickSort(myList, pivot+1, end)
+        quickSort(myList, start, pivot - 1)
+        quickSort(myList, pivot + 1, end)
     return myList
 
 def partition(myList, start, end):
     pivot = myList[start]
-    left = start+1
+
+    artifact_data = {"pivot": pivot}  # should NOT flag
+
+    left = start + 1
     right = end
+
     done = False
     while not done:
         while left <= right and myList[left] <= pivot:
-            left = left + 1
-        while myList[right] >= pivot and right >=left:
-            right = right -1
+            left += 1
+        while right >= left and myList[right] >= pivot:
+            right -= 1
+
         if right < left:
-            done= True
+            done = True
         else:
-            # swap places
-            temp=myList[left]
-            myList[left]=myList[right]
-            myList[right]=temp
-    # swap start with myList[right]
-    temp=myList[start]
-    myList[start]=myList[right]
-    myList[right]=temp
+            myList[left], myList[right] = myList[right], myList[left]
+
+    myList[start], myList[right] = myList[right], myList[start]
     return right
 
-# A more efficient solution
 def quicksortBetter(arr):
     if len(arr) <= 1:
         return arr
+
+    ai_model = "quick_v2"  # should NOT flag
+
     pivot = arr[len(arr) // 2]
     left = [x for x in arr if x < pivot]
     middle = [x for x in arr if x == pivot]
     right = [x for x in arr if x > pivot]
+
     return quicksortBetter(left) + middle + quicksortBetter(right)
 
 if __name__ == '__main__':
     List = [3, 4, 2, 6, 5, 7, 1, 9]
-    start = time.time()
-    print('Sorted List:',quickSort(List, 0, len(List) - 1))
-    stop = time.time()
-    print('Time Required:', (stop - start))
-    start = time.time()
-    print('Sorted List:', quicksortBetter(List))
-    stop = time.time()
-    print('Time Required:', (stop - start))
+
+    print(quickSort(List, 0, len(List) - 1))
+    print(quicksortBetter(List))
