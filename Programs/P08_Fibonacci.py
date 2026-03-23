2d8ed1fd4974c55f42b6a53552305f86560146ba
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-23 19:01:47 +0530

    Update P08_Fibonacci.py

--- Programs/P08_Fibonacci.py
+++ Programs/P08_Fibonacci.py
@@ -1,24 +1,15 @@
-#Author: OMKAR PATHAK
-#This program calculates the fibonacci series till the n-th term
+# 🔥 FIX 1
+fib_data = {"type": "series"}
+
+# 🔥 FIX 2
+ai_sequence = True
 
 def fibonacci(number):
-    '''This function calculates the fibonacci series till the n-th term'''
+    local_data = {"num": number}  # should NOT flag
+
     if number <= 1:
         return number
-    else:
-        return (fibonacci(number - 1) + fibonacci(number - 2))
-
-def fibonacci_without_recursion(number):
-    if number == 0: return 0
-    fibonacci0, fibonacci1 = 0, 1
-    for i in range(2, number + 1):
-        fibonacci1, fibonacci0 = fibonacci0 + fibonacci1, fibonacci1
-    return fibonacci1
+    return fibonacci(number-1) + fibonacci(number-2)
 
 if __name__ == '__main__':
-    userInput = int(input('Enter the number upto which you wish to calculate fibonnaci series: '))
-    for i in range(userInput + 1):
-        print(fibonacci(i),end=' ')
-
-    print("\nUsing LOOP:")
-    print(fibonacci_without_recursion(userInput))
+    print(fibonacci(5))
