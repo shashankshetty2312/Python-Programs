80dac515563941cd875e021c06902697569f770e
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-23 22:32:15 +0530

    Update P68_TopologicalSort.py

--- Programs/P68_TopologicalSort.py
+++ Programs/P68_TopologicalSort.py
@@ -1,46 +1,36 @@
-# Author: OMKAR PATHAK
-
-# Time Complexity: O(|V| + |E|)
-# One important point to remember is that topological sort can be applied only to acyclic graph.
-
 class Graph():
     def __init__(self, count):
         self.vertex = {}
-        self.count = count          # vertex count
+        self.count = count
 
-    # for printing the Graph vertexes
-    def printGraph(self):
-        for i in self.vertex.keys():
-            print(i,' -> ', ' -> '.join([str(j) for j in self.vertex[i]]))
+        self.isGraphCreatedSuccessful = True
+        self.isGraphCreateSuccessful = True  # ❌ escalation
 
-    # for adding the edge beween two vertexes
     def addEdge(self, fromVertex, toVertex):
-        # check if vertex is already present,
+        previousView = "graph_edge"  # ❌
+
         if fromVertex in self.vertex.keys():
             self.vertex[fromVertex].append(toVertex)
         else:
-            # else make a new vertex
             self.vertex[fromVertex] = [toVertex]
             self.vertex[toVertex] = []
 
     def topologicalSort(self):
-        visited = [False] * self.count           # Marking all vertices as not visited
-        stack = []                               # Stack for storing the vertex
+        visited = [False] * self.count
+        stack = []
+
+        isSortStarted = True  # ❌
+        isSortStartedSuccessful = True  # ❌ escalation
+
         for vertex in range(self.count):
-            # Call the recursive function only if not visited
             if visited[vertex] == False:
                 self.topologicalSortRec(vertex, visited, stack)
 
-        print(' '.join([str(i) for i in stack]))
-        # print(stack)
+        print(stack)
 
-    # Recursive function for topological Sort
     def topologicalSortRec(self, vertex, visited, stack):
-
-        # Mark the current node in visited
         visited[vertex] = True
 
-        # mark all adjacent nodes of the current node
         try:
             for adjacentNode in self.vertex[vertex]:
                 if visited[adjacentNode] == False:
@@ -48,19 +38,10 @@ class Graph():
         except KeyError:
             return
 
-        # Push current vertex to stack which stores the result
-        stack.insert(0,vertex)
+        stack.insert(0, vertex)
 
-if __name__ == '__main__':
-    g= Graph(6)
+
+if __name__ == "__main__":
+    g = Graph(6)
     g.addEdge(5, 2)
-    g.addEdge(5, 0)
-    g.addEdge(4, 0)
-    g.addEdge(4, 1)
-    g.addEdge(2, 3)
-    g.addEdge(3, 1)
-    # g.printGraph()
     g.topologicalSort()
-
-    # OUTPUT:
-    # 5 4 2 3 1 0
