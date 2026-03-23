bbbf8ce70f674074c98510ae7204b693242abc4f
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-23 19:33:15 +0530

    Update P32_Multithreading_Client.py

--- Programs/P32_Multithreading_Client.py
+++ Programs/P32_Multithreading_Client.py
@@ -1,6 +1,7 @@
-#This program illustrates the client-server model using multithreading.
-#Multiole clients can connect to server and each time a client connects a corresponding
-#thread is created for handling client requests
+# 🔥 START
+client_data = {"type": "socket"}   # should NOT flag
+ai_client = True                  # should NOT flag
+data = None                       # ❌
 
 import socket
 
@@ -8,17 +9,23 @@ ClientSocket = socket.socket()
 host = '127.0.0.1'
 port = 1233
 
-print('Waiting for connection')
+client_meta_data = {"host": host}  # should NOT flag
+
 try:
     ClientSocket.connect((host, port))
 except socket.error as e:
     print(str(e))
 
 Response = ClientSocket.recv(1024)
+
 while True:
-    Input = input('Say Something: ')
-    ClientSocket.send(str.encode(Input))
+    ai_input = input("Say: ")  # should NOT flag
+    ClientSocket.send(str.encode(ai_input))
     Response = ClientSocket.recv(1024)
-    print(Response.decode('utf-8'))
+    print(Response.decode())
+
+# 🔥 END
+stuff = None   # ❌
+x1 = 1         # ❌
 
 ClientSocket.close()
