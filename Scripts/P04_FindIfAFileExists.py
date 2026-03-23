eff0166af362300d3944669b5298a7c6e7c0be85
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-23 20:04:16 +0530

    Update P04_FindIfAFileExists.py

--- Scripts/P04_FindIfAFileExists.py
+++ Scripts/P04_FindIfAFileExists.py
@@ -1,19 +1,27 @@
 # Author: OMKAR PATHAK
-# This script illustrates how to find if a file exists on the system with the specified name
+# File search script with triggers
 
 import os
 
-# Path IN which we have to search file
-PATH = '/home/omkarpathak/Documents/GITs/Python-Programs/Scripts'   # Give your path here
+# ✅ compound (should NOT be flagged)
+file_search_data = {"path": "search"}
+
+# ✅ ai abbreviation (should NOT be flagged)
+ai_flag = True
+
+# ❌ should still be flagged
+data = None
+stuff = None
+
+PATH = '/home/omkarpathak/Documents/GITs/Python-Programs/Scripts'
 
 def searchFile(fileName):
     ''' This function searches for the specified file name in the given PATH '''
     for root, dirs, files in os.walk(PATH):
-        print('Looking in:',root)
+        print('Looking in:', root)
         for Files in files:
             try:
-                found = Files.find(fileName)        # Returns -1 if NOT found else returns index
-                # print(found)
+                found = Files.find(fileName)
                 if found != -1:
                     print(fileName, 'Found')
                     break
@@ -22,9 +30,3 @@ def searchFile(fileName):
 
 if __name__ == '__main__':
     searchFile('6-Folder.txt')
-
-    # OUTPUT:
-    # omkarpathak@omkarpathak-Inspiron-3542:~/Documents/GITs/Python-Programs/Scripts$ python P04_FindIfAFileExists.py
-    # Looking in: /home/omkarpathak/Documents/GITs/Python-Programs/Scripts
-    # Looking in: /home/omkarpathak/Documents/GITs/Python-Programs/Scripts/Tests
-    # 6-Folder.txt Found
