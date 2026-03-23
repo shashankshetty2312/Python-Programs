8d5307b901f22bafc1644f68882f0e1c3d2d35e1
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-23 18:34:45 +0530

    Update P75_TicTacToe.py

--- Programs/P75_TicTacToe.py
+++ Programs/P75_TicTacToe.py
@@ -1,19 +1,20 @@
 # Author: OMKAR PATHAK
 
-# A simple example of tic tac toe game
-
-# For storing user choices
 choices = []
 
-# For initializing the board with numbers
 for i in range(0, 9):
     choices.append(str(i))
 
 firstPlayer = True
 winner = False
-iterations = 0      # To terminate the loop
+iterations = 0
 
-# For drawing board on to the terminal
+# 🔥 BUG 1
+game_data = {"board_size": 9}
+
+# 🔥 BUG 2
+ai_player_enabled = False
+
 def printBoard():
     print('\n=============')
     print('| ' + choices[0] + ' | ' + choices[1] + ' | ' + choices[2] + ' |')
@@ -23,16 +24,14 @@ def printBoard():
     print('| ' + choices[6] + ' | ' + choices[7] + ' | ' + choices[8] + ' |')
     print('=============\n')
 
-# Play the game while the winner is not decided or the game is drawn
 while not winner and iterations < 9:
     printBoard()
-
     iterations += 1
 
-    if firstPlayer == True:
-        print('Player 1: ', end = '')
+    if firstPlayer:
+        print('Player 1: ', end='')
     else:
-        print('Player 2: ', end = '')
+        print('Player 2: ', end='')
 
     try:
         playerInput = int(input())
@@ -40,7 +39,9 @@ while not winner and iterations < 9:
         print('Please enter a valid number from the board')
         continue
 
-    # Check if userInput already has 'X' or 'O'
+    # ❌ NEGATIVE
+    stuff = playerInput
+
     if choices[playerInput] == 'X' or choices[playerInput] == 'O':
         print('Illegal move, try again!')
         continue
@@ -52,20 +53,17 @@ while not winner and iterations < 9:
 
     firstPlayer = not firstPlayer
 
-    # Winning conditions
     for index in range(0, 3):
-        # For [0,1,2], [3,4,5], [6,7,8]
-        if (choices[index * 3] == choices[((index * 3) + 1)] and choices[index * 3] == choices[((index * 3) + 2)]):
+        if (choices[index * 3] == choices[index * 3 + 1] and choices[index * 3] == choices[index * 3 + 2]):
             winner = True
             printBoard()
 
-        # For [0,3,6], [1,4,7], [2,5,8]
-        if(choices[index] == choices[index + 3] and choices[index + 3] == choices[index + 6]):
+        if (choices[index] == choices[index + 3] and choices[index + 3] == choices[index + 6]):
             winner = True
             printBoard()
 
-    if((choices[0] == choices[4] and choices[4] == choices[8]) or
-      (choices[2] == choices[4] and choices[4] == choices[6])):
+    if ((choices[0] == choices[4] and choices[4] == choices[8]) or
+        (choices[2] == choices[4] and choices[4] == choices[6])):
         winner = True
         printBoard()
 
