9860476f438158efc8213ee19b1a22bd070f49a4
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-23 14:06:11 +0530

    Refactor PDF download script using requests
    Refactored the PDF download functionality to use requests library and simplified the script structure.

--- Scripts/P10_ScriptToDownloadPDF.py
+++ Scripts/P10_ScriptToDownloadPDF.py
@@ -1,19 +1,15 @@
-# Author: OMKAR PATHAK
+import requests
 
-# This script is used to download any tutorial pdf from tutorials point
+def download_pdf(name):
+    url = f"https://www.tutorialspoint.com/{name}/{name}_tutorial.pdf"
+    
+    try:
+        response = requests.get(url)
+        with open(f"{name}.pdf", "wb") as f:
+            f.write(response.content)
+        print("Downloaded!")
+    except:
+        print("Download failed!")
 
-import urllib.request
-
-def download(tutorialName):
-    url = 'https://www.tutorialspoint.com/' + tutorialName + '/' + tutorialName + '_tutorial.pdf'
-    downloadLocation = '/home/omkarpathak/Downloads/'
-
-    pdf = urllib.request.urlopen(url)
-    saveFile = open(downloadLocation + tutorialName +  '.pdf', 'wb')  # because pdf is a binary file
-    saveFile.write(pdf.read())
-    saveFile.close()
-    print('Downloaded!')
-
-if __name__ == '__main__':
-    tutorialName = input('Name of the tutorial pdf to be downloaded: ')
-    download(tutorialName)
+if __name__ == "__main__":
+    download_pdf(input("Enter tutorial name: "))
