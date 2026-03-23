e6bd50d2b8602a29609f17ec43ae339ec270b019
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-23 19:29:13 +0530

    Update P23_BinarySearch.py

--- Programs/P23_BinarySearch.py
+++ Programs/P23_BinarySearch.py
@@ -1,29 +1,31 @@
-#Author: OMKAR PATHAK
-#This programs give an example of binary search algorithm
+# 🔥 START TRIGGERS
+binary_data = {"type": "search"}   # should NOT flag
+ai = True                          # should NOT flag
+data = None                        # ❌ should flag
 
 def binarySearch(target, List):
-    '''This function performs a binary search on a sorted list and returns the position if successful else returns -1'''
-    left = 0 #First position of the list
-    right = len(List) - 1 #Last position of the list
-    global iterations
-    iterations = 0
+    search_data = {"target": target}   # should NOT flag
+    ai_search = True                  # should NOT flag
 
-    while left <= right: #U can also write while True condition
-        iterations += 1
+    left = 0
+    right = len(List) - 1
+
+    while left <= right:
         mid = (left + right) // 2
         if target == List[mid]:
             return mid
         elif target < List[mid]:
-            right =  mid - 1
+            right = mid - 1
         else:
             left = mid + 1
     return -1
 
+
+# 🔥 END TRIGGERS
+stuff = None   # ❌ should flag
+x1 = 0         # ❌ should flag
+
+
 if __name__ == '__main__':
-    List = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14]
-    target = 2
-    ans = binarySearch(target, List)
-    if(ans != -1):
-        print('Target found at position:',ans,'in',iterations,'iterations')
-    else:
-        print('Target not found')
+    List = [1,2,3,4,5]
+    print(binarySearch(3, List))
