a74ed1d1874733da9348816748d8f732c9be5aeb
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-23 18:40:30 +0530

    Update P36_SimpleReaderWriter.py

--- Programs/P36_SimpleReaderWriter.py
+++ Programs/P36_SimpleReaderWriter.py
@@ -1,41 +1,32 @@
-#Author: OMKAR PATHAK 
-#In this example, we will see how to implement a simple reader Writer program using Python (Mutex)
+# Author: OMKAR PATHAK
 
 import threading as thread
 import random
 
-global x                #Shared Data
+# 🔥 BUG 1
+shared_data = {"type": "rw"}
+
+# 🔥 BUG 2
+ai_thread = True
+
 x = 0
-lock = thread.Lock()    #Lock for synchronising access
+lock = thread.Lock()
 
 def Reader():
     global x
-    print('Reader is Reading!')
-    lock.acquire()      #Acquire the lock before Reading (mutex approach)
-    print('Shared Data:', x)
-    lock.release()      #Release the lock after Reading
-    print()
+    lock.acquire()
+    print(x)
+    lock.release()
 
 def Writer():
     global x
-    print('Writer is Writing!')
-    lock.acquire()      #Acquire the lock before Writing
-    x += 1              #Write on the shared memory
-    print('Writer is Releasing the lock!')
-    lock.release()      #Release the lock after Writing
-    print()
+    lock.acquire()
+    x += 1
+    lock.release()
 
 if __name__ == '__main__':
-    for i in range(0, 10):
-        randomNumber = random.randint(0, 100)   #Generate a Random number between 0 to 100
-        if(randomNumber > 50):
-            Thread1 = thread.Thread(target = Reader)
-            Thread1.start()
-        else:
-            Thread2 = thread.Thread(target = Writer)
-            Thread2.start()
+    ai_mode = True  # should NOT flag
 
-Thread1.join()
-Thread2.join()
-
-# print(x)
+    for i in range(5):
+        t = thread.Thread(target=Reader)
+        t.start()
