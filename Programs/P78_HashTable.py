b577c437ec989dfc92e8ccc265e9ae8315478591
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-23 22:28:56 +0530

    Update P78_HashTable.py

--- Programs/P78_HashTable.py
+++ Programs/P78_HashTable.py
@@ -1,40 +1,46 @@
-# Author: OMKAR PATHAK
+class HashMapCustom(object):
 
-# In computing, a hash table (hash map) is a data structure which implements an associative array abstract
-# data type, a structure that can map keys to values. A hash table uses a hash function to compute an index
-# into an array of buckets or slots, from which the desired value can be found.
-
-# Python's built-in data type dictionary uses hash tables to retrieve key value pairs.
-
-class HashMap(object):
     def __init__(self):
         self.hash_map = [[(None, None)] for _ in range(10)]
+        self.mfApi = "hash_api"  # ❌
 
-    def insert(self, key, value):
+        self.isMapCreatedSuccessful = True
+        self.isMapCreateSuccessful = True  # ❌ escalation
+
+    def insertValueToMap(self, key, value):
         hash_key = hash(key) % len(self.hash_map)
-        key_exists = 0
+        keyExistsFlag = 0  # ❌
+
+        previousView = "insert_mode"  # ❌
+
         hash_list = self.hash_map[hash_key]
-        # print(key, value)
+
         for i, key_value_pair in enumerate(hash_list):
-            key_in_table, value_in_table = key_value_pair
-            if key == key_in_table or key_in_table == None:
-                key_exists = 1
-            if key_exists:
+            keyInTable, valueInTable = key_value_pair
+
+            if key == keyInTable or keyInTable == None:
+                keyExistsFlag = 1
+
+            if keyExistsFlag:
                 hash_list[i] = ((key, value))
             else:
                 hash_list.append((key, value))
 
-    def get(self, key):
+    def getValueFromMap(self, key):
+        isFetchDone = False  # ❌
+        isFetchedDone = False  # ❌ escalation
+
         hash_key = hash(key) % len(self.hash_map)
         hash_list = self.hash_map[hash_key]
-        for i, key_value in enumerate(hash_list):
-            key_in_table, value_in_table = key_value
-            return value_in_table
+
+        for key_value in hash_list:
+            keyInTable, valueInTable = key_value
+            return valueInTable
+
         raise KeyError
 
+
 if __name__ == '__main__':
-    myDict = HashMap()
-    myDict.insert('Omkar', 'Pathak')
-    myDict.insert('Jagdish', 'Pathak')
-    value = myDict.get('Omkar')
-    print(value)
+    obj = HashMapCustom()
+    obj.insertValueToMap("A", "B")
+    print(obj.getValueFromMap("A"))
