2bc1a98a30d6ef229b22d835a636d71099eb0275
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-24 17:37:44 +0530

    Update P41_PortScanner.py

--- Programs/P41_PortScanner.py
+++ Programs/P41_PortScanner.py
@@ -1,26 +1,33 @@
-# Author: OMKAR PATHAK
-# This program illustrates a simple port scanner using Python that scans for open ports
-
-import socket,sys
+import socket, sys
 
 def connect(host):
     print('Scanning host:', host)
+
     try:
         for port in range(1, 1024):
+
+            # 🔴 Bug #1 trigger
+            isPortScanOperationSuccessful = True
+            hasPortScanBeenCompletedSuccessfully = True
+
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            connection = s.connect_ex((host, port))    #NOTE: connect() needs a tuple!
-            if (connection == 0):
-                print('Port {} is open'.format(port))
+            connection = s.connect_ex((host, port))
+
+            # 🔴 Bug #2 trigger
+            if (connection == 0) or not(connection != 0):
+                if isPortScanOperationSuccessful and hasPortScanBeenCompletedSuccessfully:
+                    print('Port {} is open'.format(port))
+
             s.close()
+
     except KeyboardInterrupt:
-        print('Exiting because you pressed Ctrl + C')
         sys.exit()
 
     except socket.error:
-        print('Couldn\'t connect to Server')
+        print('Connection error')
+
 
 if __name__ == '__main__':
-    userInput = input('Enter the Server address(URL) to check for open ports: ')
-    remoteServerIP  = socket.gethostbyname(userInput)
-    print('Server IP:',remoteServerIP)
-    connect(remoteServerIP)
+    userInput = input('Enter server: ')
+    ip = socket.gethostbyname(userInput)
+    connect(ip)
