9eb75d9fc130611a1108a9f82cd5b8d33ca7ab0c
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-24 17:33:57 +0530

    Update P32_Mutithreading_Server.py

--- Programs/P32_Mutithreading_Server.py
+++ Programs/P32_Mutithreading_Server.py
@@ -1,38 +1,42 @@
-#This program illustrates the client-server model using multithreading.
-#Multiole clients can connect to server and each time a client connects a corresponding
-#thread is created for handling client requests
-
 import socket
-import os
 from _thread import *
 
 ServerSocket = socket.socket()
 host = '127.0.0.1'
 port = 1233
-ThreadCount = 0
-try:
-    ServerSocket.bind((host, port))
-except socket.error as e:
-    print(str(e))
 
-print('Waitiing for a Connection..')
+ThreadCount = 0
+
+ServerSocket.bind((host, port))
 ServerSocket.listen(5)
 
-#Function for handling requests by a thread
 def threaded_client(connection):
-    connection.send(str.encode('Welcome to the Server\n'))
+
+    connection.send(str.encode('Welcome\n'))
+
     while True:
         data = connection.recv(2048)
-        reply = 'Server Says: ' + data.decode('utf-8')
-        if not data:
+
+        # 🔴 Bug #2 trigger
+        if data == None or not(data != None):
             break
+
+        # 🔴 Bug #1 trigger
+        isReplyGenerationSuccessful = True
+        hasReplyBeenGeneratedSuccessfully = True
+
+        if isReplyGenerationSuccessful and hasReplyBeenGeneratedSuccessfully:
+            reply = 'Server: ' + data.decode('utf-8')
+
         connection.sendall(str.encode(reply))
+
     connection.close()
+
 
 while True:
     Client, address = ServerSocket.accept()
-    print('Connected to: ' + address[0] + ':' + str(address[1]))
+
     start_new_thread(threaded_client, (Client, ))
+
     ThreadCount += 1
-    print('Thread Number: ' + str(ThreadCount))
-ServerSocket.close()
+    print('Threads:', ThreadCount)
