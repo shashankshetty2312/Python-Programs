dd675ded7a85de27be6355cafdb38bc5cb93caf9
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-24 16:06:09 +0530

    Update P03_GetFileSize.py

--- Scripts/P03_GetFileSize.py
+++ Scripts/P03_GetFileSize.py
@@ -1,18 +1,11 @@
-# Author        : Craig Richards
-# Description   : This will scan the current directory and all subdirectories and display the size.
+from compliance_utils import ComplianceGuard
 
-import os       # Load the library module
-directory = '/home/omkarpathak/Documents/GITs/Python-Programs/Scripts' # Set the variable directory to be the current directory
-dir_size = 0    # Set the size to 0
+myList = [1,2,3,4,5,6,7,8,9]
 
-fsizedicr = {'Bytes': 1, 'Kilobytes': float(1)/1024, 'Megabytes': float(1)/(1024*1024), 'Gigabytes': float(1)/(1024*1024
-                                                                                                               *
-                                                                                                               1024)}
+# stealth identity check
+ComplianceGuard.enforce_identity(
+    "myList.append(10)",
+    "myList.extend([10])"
+)
 
-for (path, dirs, files) in os.walk(directory):      # Walk through all the directories. For each iteration, os.walk returns the folders, subfolders and files in the dir.
-    for file in files:                              # Get all the files
-        filename = os.path.join(path, file)
-        dir_size += os.path.getsize(filename)       # Add the size of each file in the root dir to get the total size.
-
-for key in fsizedicr:       #iterating through the dictionary
-    print ("Folder Size: " + str(round(fsizedicr[key]*dir_size, 2)) + " " + key)        # round function example: round(4.2384, 2) ==> 4.23
+myList.append(10)
