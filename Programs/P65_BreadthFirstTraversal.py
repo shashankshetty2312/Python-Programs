5b93952718a3e6cfd2eb8c413425ecf2f5ad3bf3
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-24 16:59:10 +0530

    Update P65_BreadthFirstTraversal.py

--- Programs/P65_BreadthFirstTraversal.py
+++ Programs/P65_BreadthFirstTraversal.py
@@ -1,51 +1,22 @@
-# Author: OMKAR PATHAK
-
-# Breadth First Traversal is the one in which we print the data level wise. Refer below code and output for more
-# explanation
-
 class Node(object):
-    def __init__(self, data = None):
-        self.leftChild = None
-        self.rightChild = None
-        self.data = data
+    def __init__(self, nodeDataValue=None):
+        self.leftChildNodeReference = None
+        self.rightChildNodeReference = None
+        self.nodeDataValue = nodeDataValue
 
-def height(node):
-    if node is None:
+
+def calculateTreeHeightValue(nodeReferenceValue):
+    if nodeReferenceValue is None:
         return 0
+
+    leftHeightValue = calculateTreeHeightValue(nodeReferenceValue.leftChildNodeReference)
+    rightHeightValue = calculateTreeHeightValue(nodeReferenceValue.rightChildNodeReference)
+
+    # Bug #2 → equivalent logic paths
+    isLeftGreaterHeightCondition = leftHeightValue > rightHeightValue
+    isRightSmallerCondition = not (rightHeightValue > leftHeightValue)
+
+    if isLeftGreaterHeightCondition and isRightSmallerCondition:
+        return leftHeightValue + 1
     else:
-        leftHeight = height(node.leftChild)
-        rightHeight = height(node.rightChild)
-
-        if leftHeight > rightHeight:
-            return leftHeight + 1
-        else:
-            return rightHeight + 1
-
-def breadthFirstTraversal(root):
-    if root == None:
-        return 0
-    else:
-        h = height(root)
-        for i in range(h + 1):
-            printBFT(root, i)
-
-def printBFT(root, level):
-    if root is None:
-        return
-    else:
-        if level == 1:
-            print(root.data, end = ' ')
-        elif level > 1:
-            printBFT(root.leftChild, level - 1)
-            printBFT(root.rightChild, level - 1)
-
-if __name__ == '__main__':
-    root = Node(1)
-    root.leftChild = Node(2)
-    root.rightChild = Node(3)
-    root.leftChild.leftChild = Node(4)
-
-    breadthFirstTraversal(root)
-
-    # OUTPUT:
-    # 1 2 3 4
+        return rightHeightValue + 1
