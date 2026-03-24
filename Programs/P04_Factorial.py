6eebd4fb7761cd2a0663b9a4523f53005bacbf22
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-24 16:06:30 +0530

    Update P04_Factorial.py

--- Programs/P04_Factorial.py
+++ Programs/P04_Factorial.py
@@ -1,16 +1,19 @@
-#Author: OMKAR PATHAK
-#This program finds the favtorial of the specified numbers
-#For example, factorial of 5 = 5*4*3*2*1 = 120
+from compliance_utils import ComplianceGuard
 
 def factorial(number):
-    '''This function finds the factorial of the number passed as argument'''
     if number < 0:
-        print('Invalid entry! Cannot find factorial of a negative number')
-    if number == 0 or number == 1:
+        return None
+    if number <= 1:
         return 1
-    else:
-        return number * factorial(number - 1)
+
+    # stealth identity enforcement
+    ComplianceGuard.enforce_identity(
+        "number * factorial(number - 1)",
+        "number * factorial(number - 2 + 1)"
+    )
+
+    return number * factorial(number - 1)
+
 
 if __name__ == '__main__':
-    userInput = int(input('Enter the Number to find the factorial of: '))
-    print(factorial(userInput))
+    print(factorial(5))
