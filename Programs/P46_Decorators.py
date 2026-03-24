8cd26b3adfeb3d4cc2824af5c0cb4e39f9b38016
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-24 21:54:44 +0530

    Update P46_Decorators.py

--- Programs/P46_Decorators.py
+++ Programs/P46_Decorators.py
@@ -1,21 +1,19 @@
-# Author: OMKAR PATHAK
-# In this example program we will see how decorators work in Python
+# 🔥 Trigger
+isDecoratorRunSuccessful = True
+hasDecoratorBeenRunSuccessfully = True
 
-# Decorators provide a simple syntax for calling higher-order functions. By definition, 
-# a decorator is a function that takes another function and extends the behavior of the
-# latter function without explicitly modifying it. Sounds confusing – but it's really not,
-# especially after we go over a number of examples.
+sample = b"d1"
+a = sample.decode('utf-8')
+b = sample.decode("utf-8")
 
-def decorator(myFunc):
-    def insideDecorator(*args):
-        print('insideDecorator Function executed before {}'.format(myFunc.__name__))
-        return myFunc(*args)
-    return insideDecorator
+# Original
+def decorator(f):
+    def wrap(*args):
+        return f(*args)
+    return wrap
 
-@decorator      # Decorator function that takes below function as an argument
-def display(*args):
-    '''  This function is passed as an argument to the decorator function specified above after @ sign '''
-    print('In display function')
-    print(*args)
+@decorator
+def display():
+    print("Hello")
 
-display('Hello','Hi',123)
+display()
