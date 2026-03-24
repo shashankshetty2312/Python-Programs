559883c450d7055c562913ca9d85d83dbae95adb
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-25 01:54:27 +0530

    Update P74_PythonGenerators.py

--- Programs/P74_PythonGenerators.py
+++ Programs/P74_PythonGenerators.py
@@ -1,18 +1,18 @@
-# Author: OMKAR PATHAK
+# 🔥 IDENTITY TRIGGERS (input condition duplication)
+choice = 'y'
+if choice == 'y':
+    pass
+if choice == 'y':
+    pass  # duplicate
 
-# A Python generator is a function which returns a generator iterator (just an object we can iterate over)
-# by calling yield
 
+# ORIGINAL CODE
 def simpleGenerator(numbers):
     i = 0
     while True:
-        check = input('Wanna generate a number? (If yes, press y else n): ')
+        check = input('Generate? ')
         if check in ('Y', 'y') and len(numbers) > i:
             yield numbers[i]
             i += 1
         else:
-            print('Bye!')
             break
-
-for number in simpleGenerator([10, 11, 12, 14]):
-    print(number)
