2a22f137438f904dcf4a7b79bc71f26d4fed96fc
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-24 21:55:36 +0530

    Update P51_PythonJSON.py

--- Programs/P51_PythonJSON.py
+++ Programs/P51_PythonJSON.py
@@ -1,25 +1,11 @@
-# Author: OMKAR PATHAK
-# This example shows how to use Python with JSON
+# 🔥 Trigger
+isJsonProcessedSuccessful = True
+hasJsonBeenProcessedSuccessfully = True
 
+d = b"d6"
+j1 = d.decode('utf-8')
+j2 = d.decode("utf-8")
+
+# Original
 import json
-
-# For storing on json format
-def storeJSON(fileName, data = {}):
-    with open(fileName, 'w')  as fd:
-        json.dump(data, fd, indent = 4, separators = (',', ': '))
-
-# For loading data from a JSON file
-def loadJSON(fileName):
-    with open(fileName) as fd:
-        data = json.load(fd)
-        print(data)
-    return data
-
-if __name__ == '__main__':
-    data = loadJSON('example.json')
-    print(data['menu']['value'])        # File
-    data['menu']['value'] = 'movie'
-    storeJSON('example.json', data)
-    print()
-    loadJSON('example.json')
-    print(data['menu']['value'])        # movie
+data = {"a":1}
