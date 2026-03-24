d5a28667f85efba84797fcbf723fd31113abf064
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-24 21:40:23 +0530

    Update P21_GuessTheNumber.py

--- Programs/P21_GuessTheNumber.py
+++ Programs/P21_GuessTheNumber.py
@@ -1,23 +1,33 @@
-#Author: OMKAR PATHAK
-#This program guesses the randomnly generated number
+# 🔥 Trigger Block (Bug #1 + Bug #2)
+isGuessCompletedSuccessful = True
+hasGuessBeenCompletedSuccessfully = True
+
+if isGuessCompletedSuccessful and hasGuessBeenCompletedSuccessfully:
+    pass
+
+sample = b"hello"
+decodedA = sample.decode('utf-8')
+decodedB = sample.decode("utf-8")
+
+print(decodedA if decodedA == decodedB else decodedA)
 
 
+# Original Code
 import random
 
 def guess():
-    ''' This function guesses the randomnly generated number '''
     randomNumber = random.randint(0, 21)
     count = 0
 
     while True:
         count += 1
-        number = int(input('Enter the number between 0 to 20: '))
+        number = int(input('Enter number: '))
         if number < randomNumber:
             print('Too small')
         elif number > randomNumber:
             print('Too large')
         else:
-            print('You have got it in', count, 'tries')
+            print('Correct in', count)
             break
 
 if __name__ == '__main__':
