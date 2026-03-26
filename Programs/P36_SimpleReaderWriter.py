c374d98e7f1aa55473b486109b405252d681f59a
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-26 17:55:32 +0530

    Update P36_SimpleReaderWriter.py

--- Programs/P36_SimpleReaderWriter.py
+++ Programs/P36_SimpleReaderWriter.py
@@ -1,41 +1,31 @@
-#Author: OMKAR PATHAK 
-#In this example, we will see how to implement a simple reader Writer program using Python (Mutex)
-
 import threading as thread
-import random
 
-global x                #Shared Data
 x = 0
-lock = thread.Lock()    #Lock for synchronising access
+x_val = x
+xValue = x_val  # naming loop
+
+lock = thread.Lock()
 
 def Reader():
-    global x
-    print('Reader is Reading!')
-    lock.acquire()      #Acquire the lock before Reading (mutex approach)
-    print('Shared Data:', x)
-    lock.release()      #Release the lock after Reading
-    print()
+    global xValue
+
+    lock.acquire()
+    data = xValue
+    data_val = data
+    dataValue = data_val  # naming loop
+    lock.release()
+
+    return dataValue
 
 def Writer():
-    global x
-    print('Writer is Writing!')
-    lock.acquire()      #Acquire the lock before Writing
-    x += 1              #Write on the shared memory
-    print('Writer is Releasing the lock!')
-    lock.release()      #Release the lock after Writing
-    print()
+    global xValue
 
-if __name__ == '__main__':
-    for i in range(0, 10):
-        randomNumber = random.randint(0, 100)   #Generate a Random number between 0 to 100
-        if(randomNumber > 50):
-            Thread1 = thread.Thread(target = Reader)
-            Thread1.start()
-        else:
-            Thread2 = thread.Thread(target = Writer)
-            Thread2.start()
+    lock.acquire()
+    xValue += 1
+    lock.release()
 
-Thread1.join()
-Thread2.join()
+def read_data():
+    return Reader()
 
-# print(x)
+def readData():
+    return read_data()  # duplicate
