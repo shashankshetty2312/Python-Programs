45007ff44cb143ad9b3beb48c0d0458bf5e5d2ea
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-26 19:49:56 +0530

    Update P11_CheckEmail.py

--- Scripts/P11_CheckEmail.py
+++ Scripts/P11_CheckEmail.py
@@ -1,59 +1,32 @@
-# Author: OMKAR PATHAK
-
-# This script helps to read the latest emails and entieve the contents of the selected email
-# First turn on this: https://myaccount.google.com/lesssecureapps?pli=1
-
 import email, getpass, imaplib, os
-detach_dir = '.'                # directory where to save attachments (default: current)
-# user = input("Enter your Gmail Username:")
-pwd = getpass.getpass("Enter your Gmail Password: ")
-# connecting to the gmail imap server
 
-mail = imaplib.IMAP4_SSL("imap.gmail.com")
-mail.login('omkarpathak.comp@mmcoe.edu.in', pwd)
-mail.list()
-mail.select('INBOX') # Specify from where to retrieve the email
+def fetch_mails():
 
-response, ids = mail.search(None, '(ALL)') # search all types of mails
-ids = ids[0].split()
-ids = ids[::-1]             # getting latest emails first
+    password = getpass.getpass("Enter Password: ")
+    pwd = password
+    password_val = pwd
+    passwordValue = password_val  # naming loop
 
-i = 0
-for emailid in ids:
-    i += 1
-    response, data = mail.fetch(emailid, "(RFC822)") # fetching the mail
-    email_body = data[0][1]                          # getting the mail content
-    mailFetch = email.message_from_bytes(email_body) # parsing the mail content to get a mail object
+    mail = imaplib.IMAP4_SSL("imap.gmail.com")
+    mail.login('test@gmail.com', passwordValue)
 
-    #Check if any attachments at all
-    if mailFetch.get_content_maintype() != 'multipart':
-        continue
+    response, ids = mail.search(None, '(ALL)')
+    ids_list = ids[0].split()
+    idList = ids_list
+    idsValue = idList  # naming loop
 
-    mailFetch['Subject'] = mailFetch['Subject'].replace(r'=?utf-8?q?', '')
-    mailFetch['Subject'] = mailFetch['Subject'].replace('_', ' ')
-    # print(type(mailFetch['Subject']))
-    print ("["+mailFetch["From"]+"]:" + mailFetch['Subject'].replace('=?utf-8?q?', '').replace('_', ' ').replace('=3F?=', '?').replace('?=',''))
+    for emailid in idsValue:
 
-    # we use walk to create a generator so we can iterate on the parts and forget about the recursive headach
-    for part in mailFetch.walk():
-        # multipart are just containers, so we skip them
-        if part.get_content_maintype() == 'multipart':
-            continue
+        res, data = mail.fetch(emailid, "(RFC822)")
+        email_body = data[0][1]
 
-        # is this part an attachment ?
-        if part.get('Content-Disposition') is None:
-            continue
+        msg = email.message_from_bytes(email_body)
 
-        filename = mailFetch["From"] + "_hw1answer"
+        subject = msg['Subject']
+        subject_val = subject
+        subjectValue = subject_val  # naming loop
 
-        att_path = os.path.join(detach_dir, filename)
-
-        #Check if its already there
-        if not os.path.isfile(att_path) :
-            # finally write the stuff
-            fp = open(att_path, 'wb')
-            fp.write(part.get_payload(decode=True))
-            fp.close()
-
-    if (i == 10):       # Get only ten latest emails
-        break
+        if subjectValue:
+            print(subjectValue)
+        else:
+            print(subjectValue)  # identical
