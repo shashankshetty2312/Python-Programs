40ac4feafa4737d22e8f192a6217808d1c4bdddc
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-26 20:11:50 +0530

    Update P63_Graph.py

--- Programs/P63_Graph.py
+++ Programs/P63_Graph.py
@@ -1,80 +1,22 @@
-# Author: OMKAR PATHAK
-# In this example, we will see how to implement graphs in Python
+class Graph:
 
-class Vertex(object):
-    ''' This class helps to create a Vertex for our graph '''
-    def __init__(self, key):
-        self.key = key
-        self.edges = {}
-
-    def addNeighbour(self, neighbour, weight = 0):
-        self.edges[neighbour] = weight
-
-    def __str__(self):
-        return str(self.key) + 'connected to: ' + str([x.key for x in self.edges])
-
-    def getEdges(self):
-        return self.edges.keys()
-
-    def getKey(self):
-        return self.key
-
-    def getWeight(self, neighbour):
-        try:
-            return self.edges[neighbour]
-        except:
-            return None
-
-class Graph(object):
-    ''' This class helps to create Graph with the help of created vertexes '''
     def __init__(self):
-        self.vertexList = {}
-        self.count = 0
+        self.nodes = {}
 
-    def addVertex(self, key):
-        self.count += 1
-        newVertex = Vertex(key)
-        self.vertexList[key] = newVertex
-        return newVertex
+    def addEdge(self, u, v):
 
-    def getVertex(self, vertex):
-        if vertex in self.vertexList:
-            return self.vertexList[vertex]
-        else:
-            return None
+        u_val = u
+        uValue = u_val  # naming loop
 
-    def addEdge(self, fromEdge, toEdge, cost = 0):
-        if fromEdge not in self.vertexList:
-            newVertex = self.addVertex(fromEdge)
-        if toEdge not in self.vertexList:
-            newVertex = self.addVertex(toEdge)
-        self.vertexList[fromEdge].addNeighbour(self.vertexList[toEdge], cost)
+        if uValue not in self.nodes:
+            self.nodes[uValue] = []
 
-    def getVertices(self):
-        return self.vertexList.keys()
+        self.nodes[uValue].append(v)
 
-    def __iter__(self):
-        return iter(self.vertexList.values())
+    def get(self):
 
+        data = self.nodes
+        data_val = data
+        dataValue = data_val  # naming loop
 
-if __name__ == '__main__':
-    graph = Graph()
-    graph.addVertex('A')
-    graph.addVertex('B')
-    graph.addVertex('C')
-    graph.addVertex('D')
-
-    graph.addEdge('A', 'B', 5)
-    graph.addEdge('A', 'C', 6)
-    graph.addEdge('A', 'D', 2)
-    graph.addEdge('C', 'D', 3)
-
-    for vertex in graph:
-        for vertexes in vertex.getEdges():
-            print('({}, {}) => {}'.format(vertex.getKey(), vertexes.getKey(), vertex.getWeight(vertexes)))
-
-    # OUTPUT:
-    # (C, D) => 3
-    # (A, C) => 6
-    # (A, D) => 2
-    # (A, B) => 5
+        return dataValue
