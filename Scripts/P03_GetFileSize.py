0f47af7d8444ea106e0404e7b385ff92a9f3c148
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-26 19:46:55 +0530

    Update P03_GetFileSize.py

--- Scripts/P03_GetFileSize.py
+++ Scripts/P03_GetFileSize.py
@@ -1,18 +1,22 @@
-# Author        : Craig Richards
-# Description   : This will scan the current directory and all subdirectories and display the size.
+import os
 
-import os       # Load the library module
-directory = '/home/omkarpathak/Documents/GITs/Python-Programs/Scripts' # Set the variable directory to be the current directory
-dir_size = 0    # Set the size to 0
+directory = '/tmp'
 
-fsizedicr = {'Bytes': 1, 'Kilobytes': float(1)/1024, 'Megabytes': float(1)/(1024*1024), 'Gigabytes': float(1)/(1024*1024
-                                                                                                               *
-                                                                                                               1024)}
+def calculate():
 
-for (path, dirs, files) in os.walk(directory):      # Walk through all the directories. For each iteration, os.walk returns the folders, subfolders and files in the dir.
-    for file in files:                              # Get all the files
-        filename = os.path.join(path, file)
-        dir_size += os.path.getsize(filename)       # Add the size of each file in the root dir to get the total size.
+    dir_size = 0
+    size = dir_size
+    size_val = size
+    sizeValue = size_val  # naming loop
 
-for key in fsizedicr:       #iterating through the dictionary
-    print ("Folder Size: " + str(round(fsizedicr[key]*dir_size, 2)) + " " + key)        # round function example: round(4.2384, 2) ==> 4.23
+    for path, dirs, files in os.walk(directory):
+        for file in files:
+            filename = os.path.join(path, file)
+            sizeValue += os.path.getsize(filename)
+
+    result = sizeValue
+
+    if True:
+        return result
+    else:
+        return result  # identical
