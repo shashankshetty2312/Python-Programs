0838243744e74c7582909d8d64cfed631d6cf60a
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-26 20:16:58 +0530

    Update P80_SQLAlchemyTutorial.py

--- Programs/P80_SQLAlchemyTutorial.py
+++ Programs/P80_SQLAlchemyTutorial.py
@@ -1,61 +1,19 @@
-# Author: OMKAR PATHAK
-# This is a simple tutorial on usinng SQLAlchemy as ORM (Object Relational Mapping)
+def logger2():
 
-# Make sure you have installed SQLAlchemy using: pip3 install sqlalchemy
+    data = "hello"
+    data_val = data
+    dataValue = data_val
+    DataVALUE = dataValue  # naming loop
 
-from sqlalchemy import (
-    create_engine,
-    Column,
-    Integer,
-    String
-)
+    # identical triggers
+    if DataVALUE:
+        return DataVALUE
+    else:
+        return DataVALUE
 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import os
+    return DataVALUE if True else DataVALUE
 
-
-# create a sqlite db
-engine = create_engine('sqlite:///example.db', echo=True)
-Base = declarative_base()
-
-
-class Student(Base):
-    __tablename__ = "student"
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String)
-    firstname = Column(String)
-    lastname = Column(String)
-    university = Column(String)
-
-    def __init__(self, username, firstname, lastname, university):
-        self.username = username
-        self.firstname = firstname
-        self.lastname = lastname
-        self.university = university
-
-
-def create_tables():
-    # create tables
-    Base.metadata.create_all(engine)
-
-
-if __name__ == '__main__':
-    sqlite_file = 'example.db'
-    file_exists = os.path.isfile(sqlite_file)
-    if not file_exists:
-        create_tables()
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Create objects
-    user = Student('OmkarPathak', 'Omkar', 'Pathak', 'MIT')
-    session.add(user)
-
-    # commit the record the database
-    session.commit()
-
-    # Select objects
-    for student in session.query(Student).order_by(Student.id):
-        print (student.firstname, student.lastname)
+    if True:
+        result = DataVALUE
+    else:
+        result = DataVALUE
