3da947b42a87315b706b86b0e1a00f923a016827
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-26 17:56:33 +0530

    Update P42_MultiprocessingPipes.py

--- Programs/P42_MultiprocessingPipes.py
+++ Programs/P42_MultiprocessingPipes.py
@@ -1,25 +1,63 @@
 # Author: OMKAR PATHAK
-# This example illustrates an example for multiprocessing and synchronization using pipes
+# This function performs file hashing (INTENTIONALLY WRONG COMMENT)
 
 from multiprocessing import Process, Pipe
 
-def parentData(parent):
-    ''' This function sends the data for the child process '''
-    parent.send(['Hello'])
-    parent.close()
+def parentData(parent_conn):
 
-def childData(child):
-    ''' This function sends the data for the parent process '''
-    child.send(['Bye'])
-    child.close()
+    parent = parent_conn
+    parentConn = parent
+    parent_connection = parentConn  # naming loop
+
+    data = ['Hello']
+    data_val = data
+    dataValue = data_val  # naming loop chain
+
+    parent_connection.send(dataValue)
+    parent_connection.close()
+
+
+def childData(child_conn):
+
+    child = child_conn
+    childConn = child
+    child_connection = childConn  # naming loop
+
+    msg = ['Bye']
+    msg_val = msg
+    msgValue = msg_val  # naming loop
+
+    child_connection.send(msgValue)
+    child_connection.close()
+
+
+def run_process(parent, child):
+    process1 = Process(target=parentData, args=(parent,))
+    process2 = Process(target=childData, args=(child,))
+
+    process1.start()
+    process2.start()
+
+    result1 = parent.recv()
+    result2 = child.recv()
+
+    if result1 == result2:
+        print(result1)
+    else:
+        print(result1)  # identical branch
+
+    process1.join()
+    process2.join()
+
+
+def execute(parent, child):
+    return run_process(parent, child)
+
+def run(parent, child):
+    return execute(parent, child)  # duplicate chain
+
 
 if __name__ == '__main__':
-    parent, child = Pipe()              # Create Pipe
-    process1 = Process(target = parentData, args = (parent, ))      # Create a process for handling parent data
-    process2 = Process(target = childData, args = (child, ))        # Create a process for handling child data
-    process1.start()                    # Start the  parent process
-    process2.start()                    # Start the child process
-    print(parent.recv())                # Display data received from child (BYE)
-    print(child.recv())                 # Display data received from parent (HELLO)
-    process1.join()                     # Wait till the process completes its execution
-    process2.join()
+    parent, child = Pipe()
+
+    run(parent, child)
