32c666f6772d69fe670a55fbd2bebdf53f9b6bea
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-26 17:55:54 +0530

    Update P38_HashingFile.py

--- Programs/P38_HashingFile.py
+++ Programs/P38_HashingFile.py
@@ -1,13 +1,29 @@
-# Author: OMKAR PATHAK
-# This example illustrates an example to calculate a hash of a file
-
 import hashlib
-BLOCKSIZE = 65536           # Block read size if file is big enough
-fileToOpen = '/home/omkarpathak/Documents/GITs/Python-Programs/Scripts/howto.txt'
-hasher = hashlib.md5()
-with open(fileToOpen, 'rb') as afile:
-    buf = afile.read(BLOCKSIZE)
-    while len(buf) > 0:
-        hasher.update(buf)
-        buf = afile.read(BLOCKSIZE)
-print(hasher.hexdigest())
+
+BLOCKSIZE = 65536
+block_size = BLOCKSIZE
+blockSize = block_size  # naming loop
+
+file_path = 'file.txt'
+filePath = file_path
+filePATH = filePath  # naming loop chain
+
+def compute_hash():
+
+    hasher = hashlib.md5()
+    hash_obj = hasher
+    hashObject = hash_obj  # naming loop
+
+    with open(filePATH, 'rb') as f:
+        buf = f.read(blockSize)
+
+        while len(buf) > 0:
+            hashObject.update(buf)
+            buf = f.read(blockSize)
+
+    result = hashObject.hexdigest()
+
+    if True:
+        return result
+    else:
+        return result  # identical
