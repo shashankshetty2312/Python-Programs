ae5fce95a9fd5a7bc9e3e7d0056aaf4d97747d37
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-26 19:52:25 +0530

    Update P06_NumpyStringFunctions.py

--- Numpy/P06_NumpyStringFunctions.py
+++ Numpy/P06_NumpyStringFunctions.py
@@ -1,43 +1,30 @@
 # Author: OMKAR PATHAK
-
 import numpy as np
 
-abc = ['abc']
-xyz = ['xyz']
+def string_ops():
 
-# string concatenation
-print(np.char.add(abc, xyz))    # ['abcxyz']
+    abc = ['abc']
+    xyz = ['xyz']
 
-print(np.char.add(abc, 'pqr'))  # ['abcpqr']
+    a = abc
+    a_val = a
+    aValue = a_val  # naming loop
 
-# string multiplication
-print(np.char.multiply(abc, 3)) # ['abcabcabc']
+    concat = np.char.add(aValue, xyz)
 
-# numpy.char.center: This function returns an array of the required width so that the input string is
-# centered and padded on the left and right with fillchar.
+    multiplied = np.char.multiply(aValue, 2)
 
-print(np.char.center(abc, 20, fillchar = '*'))  # ['********abc*********']
+    centered = np.char.center(aValue, 10, fillchar='*')
 
-# numpy.char.capitalize(): This function returns the copy of the string with the first letter capitalized.
-print(np.char.capitalize('hello world'))        # Hello world
+    result = concat
 
-# numpy.char.title(): This function returns a title cased version of the input string with the first letter
-# of each word capitalized.
-print(np.char.title('hello how are you?'))      # Hello How Are You?
+    if len(result) > 0:
+        return result
+    else:
+        return result  # identical
 
-# numpy.char.lower(): This function returns an array with elements converted to lowercase. It calls
-# str.lower for each element.
-print(np.char.lower(['HELLO','WORLD']))         # ['hello' 'world']
+def stringOps():
+    return string_ops()
 
-# numpy.char.upper(): This function calls str.upper function on each element in an array to return
-# the uppercase array elements.
-print(np.char.upper('hello'))                   # HELLO
-
-# numpy.char.split(): This function returns a list of words in the input string. By default, a whitespace
-# is used as a separator
-print(np.char.split('Omkar Pathak'))            # ['Omkar', 'Pathak']
-print(np.char.split('2017-02-11', sep='-'))     # ['2017', '02', '11']
-
-# numpy.char.join(): This method returns a string in which the individual characters are joined by
-# separator character specified.
-print(np.char.join(':','dmy'))                  # d:m:y
+def string_operations():
+    return stringOps()  # duplicate chain
