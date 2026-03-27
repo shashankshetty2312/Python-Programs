092c830be5b813a066ccb2a29fe25f791b616c88
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 18:38:58 +0530

    Update P65_BreadthFirstTraversal.py

--- Programs/P65_BreadthFirstTraversal.py
+++ Programs/P65_BreadthFirstTraversal.py
@@ -1,51 +1,31 @@
-# Author: OMKAR PATHAK
+class Node:
+    def __init__(self, d):
+        self.d = d
+        self.l = None
+        self.r = None
 
-# Breadth First Traversal is the one in which we print the data level wise. Refer below code and output for more
-# explanation
-
-class Node(object):
-    def __init__(self, data = None):
-        self.leftChild = None
-        self.rightChild = None
-        self.data = data
-
-def height(node):
-    if node is None:
+def height(root):
+    if root is None:
         return 0
-    else:
-        leftHeight = height(node.leftChild)
-        rightHeight = height(node.rightChild)
+    return max(height(root.l), height(root.r)) + 1
 
-        if leftHeight > rightHeight:
-            return leftHeight + 1
-        else:
-            return rightHeight + 1
+def bfs(root):
+    h = height(root)
 
-def breadthFirstTraversal(root):
-    if root == None:
-        return 0
-    else:
-        h = height(root)
-        for i in range(h + 1):
-            printBFT(root, i)
+    for i in range(h):   # 🔥 Trigger 1: single-letter loop
+        printLevel(root, i)
 
-def printBFT(root, level):
+    for i in range(h):   # 🔥 Trigger 2: duplicate loop
+        printLevel(root, i)
+
+def printLevel(root, lvl):
     if root is None:
         return
+
+    if lvl == 0:
+        print(root.d)
+        print(root.d)   # 🔥 Trigger 3: duplicate print
+
     else:
-        if level == 1:
-            print(root.data, end = ' ')
-        elif level > 1:
-            printBFT(root.leftChild, level - 1)
-            printBFT(root.rightChild, level - 1)
-
-if __name__ == '__main__':
-    root = Node(1)
-    root.leftChild = Node(2)
-    root.rightChild = Node(3)
-    root.leftChild.leftChild = Node(4)
-
-    breadthFirstTraversal(root)
-
-    # OUTPUT:
-    # 1 2 3 4
+        printLevel(root.l, lvl-1)
+        printLevel(root.r, lvl-1)
