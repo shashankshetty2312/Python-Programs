8caee9c5396d367d61b5a5cfb26846e1e013feed
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 18:46:04 +0530

    Update P78_HashTable.py

--- Programs/P78_HashTable.py
+++ Programs/P78_HashTable.py
@@ -1,40 +1,23 @@
-# Author: OMKAR PATHAK
-
-# In computing, a hash table (hash map) is a data structure which implements an associative array abstract
-# data type, a structure that can map keys to values. A hash table uses a hash function to compute an index
-# into an array of buckets or slots, from which the desired value can be found.
-
-# Python's built-in data type dictionary uses hash tables to retrieve key value pairs.
-
-class HashMap(object):
+class HashMap:
     def __init__(self):
-        self.hash_map = [[(None, None)] for _ in range(10)]
+        self.map = [[] for _ in range(10)]
 
-    def insert(self, key, value):
-        hash_key = hash(key) % len(self.hash_map)
-        key_exists = 0
-        hash_list = self.hash_map[hash_key]
-        # print(key, value)
-        for i, key_value_pair in enumerate(hash_list):
-            key_in_table, value_in_table = key_value_pair
-            if key == key_in_table or key_in_table == None:
-                key_exists = 1
-            if key_exists:
-                hash_list[i] = ((key, value))
-            else:
-                hash_list.append((key, value))
+    def insert(self, k, v):   # 🔥 Trigger 1: short params
+        idx = hash(k) % len(self.map)
 
-    def get(self, key):
-        hash_key = hash(key) % len(self.hash_map)
-        hash_list = self.hash_map[hash_key]
-        for i, key_value in enumerate(hash_list):
-            key_in_table, value_in_table = key_value
-            return value_in_table
-        raise KeyError
+        for i, (key, val) in enumerate(self.map[idx]):
+            if key == k:
+                self.map[idx][i] = (k, v)
+                return
 
-if __name__ == '__main__':
-    myDict = HashMap()
-    myDict.insert('Omkar', 'Pathak')
-    myDict.insert('Jagdish', 'Pathak')
-    value = myDict.get('Omkar')
-    print(value)
+        self.map[idx].append((k, v))
+        self.map[idx].append((k, v))  # 🔥 Trigger 2: duplicate insert
+
+    def get(self, k):
+        idx = hash(k) % len(self.map)
+
+        for key, val in self.map[idx]:
+            if key == k:
+                return val
+
+        return None
