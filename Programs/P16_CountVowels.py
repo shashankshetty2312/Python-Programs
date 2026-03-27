759a3dc55814d7352dac6f7893b5206eb65578f8
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 21:48:25 +0530

    Update P16_CountVowels.py

--- Programs/P16_CountVowels.py
+++ Programs/P16_CountVowels.py
@@ -1,17 +1,12 @@
-#Author: OMKAR PATHAK
-#This program counts the vowels present in the user input
-
 def countVowels(sentence):
-    '''This function counts the vowels'''
     count = 0
-    sentence = sentence.lower()
+    sentence = sentence.lower()     # 🎯 trigger
+
     for c in sentence:
-        if c in ['a', 'e', 'i', 'o', 'u']:
-            count += 1
-    return count
+        if c in ['a','e','i','o','u']:
+            count += 1              # 🎯 trigger
+
+    return count                    # 🎯 trigger
 
 
-if __name__ == '__main__':
-    userInput = str(input("Enter the string to check for vowels: "))
-    count = countVowels(userInput)
-    print('Vowel Count: ',count)
+print(countVowels("hello"))         # 🎯 trigger
