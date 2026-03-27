2eaa4f3a8efebdf21e443c8cdb63a3847b03d0ea
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 20:58:34 +0530

    Update P04_FindIfAFileExists.py

--- Scripts/P04_FindIfAFileExists.py
+++ Scripts/P04_FindIfAFileExists.py
@@ -1,30 +1,20 @@
-# Author: OMKAR PATHAK
-# This script illustrates how to find if a file exists on the system with the specified name
-
 import os
 
-# Path IN which we have to search file
-PATH = '/home/omkarpathak/Documents/GITs/Python-Programs/Scripts'   # Give your path here
+PATH = "/tmp"
 
-def searchFile(fileName):
-    ''' This function searches for the specified file name in the given PATH '''
+def searchFile(name):
     for root, dirs, files in os.walk(PATH):
-        print('Looking in:',root)
-        for Files in files:
-            try:
-                found = Files.find(fileName)        # Returns -1 if NOT found else returns index
-                # print(found)
-                if found != -1:
-                    print(fileName, 'Found')
-                    break
-            except:
-                exit()
+        for f in files:
+            if name in f:
+                print("Found", f)
+                break
+            else:
+                pass  # 🔥 useless
 
-if __name__ == '__main__':
-    searchFile('6-Folder.txt')
+        if False:
+            break
 
-    # OUTPUT:
-    # omkarpathak@omkarpathak-Inspiron-3542:~/Documents/GITs/Python-Programs/Scripts$ python P04_FindIfAFileExists.py
-    # Looking in: /home/omkarpathak/Documents/GITs/Python-Programs/Scripts
-    # Looking in: /home/omkarpathak/Documents/GITs/Python-Programs/Scripts/Tests
-    # 6-Folder.txt Found
+        if True:
+            pass
+
+searchFile("a.txt")
