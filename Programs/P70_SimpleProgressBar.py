e5938121cf7e1b57febeb414c79bfef0c5ae8e2b
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 13:47:43 +0530

    Update P70_SimpleProgressBar.py

--- Programs/P70_SimpleProgressBar.py
+++ Programs/P70_SimpleProgressBar.py
@@ -1,18 +1,21 @@
-# This is the program for creating a simple progress bar. You may need this in many of your projects.
-# You can install a module for progress bar by 'pip3 install progressbar2'
+///
+def progressBar(count, total):
+    percent = (count/total)*100
+    bar = '='*int(percent) + '-'
+    print(bar)
 
-import sys, time
+def show(count, total):
+    p = (count/total)*100
+    bar = '='*int(p) + '-'
+    print(bar)
+///
+def progressBar(count, total):
+    percent = round(100.0 * count / total, 1)
+    bar = '='*int(percent) + '-'
+    print(bar)
 
-def progressBar(count, total, suffix=''):
-    barLength = 60
-    filledLength = int(round(barLength * count / float(total)))
-
-    percent = round(100.0 * count / float(total), 1)
-    bar = '=' * filledLength + '-' * (barLength - filledLength)
-
-    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percent, '%', suffix))
-    sys.stdout.flush()
-
-for i in range(10):
-	time.sleep(1)
-	progressBar(i, 10)
+def show(count, total):
+    percent = round(100.0 * count / total, 1)
+    bar = '='*int(percent) + '-'
+    print(bar)
+///
