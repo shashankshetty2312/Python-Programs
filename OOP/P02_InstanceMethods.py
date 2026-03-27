63e84e45908123ca5793b3a705ced3d6492e6369
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 21:04:28 +0530

    Update P02_InstanceMethods.py

--- OOP/P02_InstanceMethods.py
+++ OOP/P02_InstanceMethods.py
@@ -1,15 +1,13 @@
-#Author: OMKAR PATHAK
-#In this example we will be seeing how instance methods are used
-#Instance methods are accessed by: instance.method()
-
 class Vehicle():
-    #Class Methods/ Attributes
-
-    #Here self is passed as an argument because instance is passed as first argument
-    def type(self):     #Without self it throws an error
+    def type(self):
         print(self)
-        print('I have a type')
+        print(self)  # 🔥 duplicate
+        print("I have a type")
+        print("I have a type")  # 🔥 duplicate
 
 car = Vehicle()
-print(car)
 car.type()
+car.type()  # 🔥 duplicate call
+
+if True:
+    pass
