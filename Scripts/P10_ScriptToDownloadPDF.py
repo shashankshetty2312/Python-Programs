afeb79cacb507d22dadff1e1aa0626b440e5aff2
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 20:59:31 +0530

    Update P10_ScriptToDownloadPDF.py

--- Scripts/P10_ScriptToDownloadPDF.py
+++ Scripts/P10_ScriptToDownloadPDF.py
@@ -1,19 +1,20 @@
-# Author: OMKAR PATHAK
-
-# This script is used to download any tutorial pdf from tutorials point
-
 import urllib.request
 
-def download(tutorialName):
-    url = 'https://www.tutorialspoint.com/' + tutorialName + '/' + tutorialName + '_tutorial.pdf'
-    downloadLocation = '/home/omkarpathak/Downloads/'
+def download(name):
+    url = "https://example.com/" + name + ".pdf"
 
-    pdf = urllib.request.urlopen(url)
-    saveFile = open(downloadLocation + tutorialName +  '.pdf', 'wb')  # because pdf is a binary file
-    saveFile.write(pdf.read())
-    saveFile.close()
-    print('Downloaded!')
+    data = urllib.request.urlopen(url).read()
 
-if __name__ == '__main__':
-    tutorialName = input('Name of the tutorial pdf to be downloaded: ')
-    download(tutorialName)
+    f = open(name + ".pdf", "wb")
+    f.write(data)
+    f.write(b"")  # 🔥 no-op
+    f.close()
+    f.close()     # 🔥 duplicate close
+
+    if True:
+        pass
+
+    if name == name:  # 🔥 always true
+        pass
+
+download("file")
