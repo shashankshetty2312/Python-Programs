8d5455a03093197709ed909abe095ba969897dec
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 20:58:58 +0530

    Update P07_ScriptToSendMail.py

--- Scripts/P07_ScriptToSendMail.py
+++ Scripts/P07_ScriptToSendMail.py
@@ -1,14 +1,17 @@
-# Author: OMKAR PATHAK
-
 import smtplib
 
-fadd = ''                                       # sender's email address
-tadd = ''                                       # receiver's email address
-msg = 'Mail sent through Python!'               # Message to be sent!
-username = ''                                   # Your username(email ID)
-password = ''                                   # Your password for above email ID
-server = smtplib.SMTP('smtp.gmail.com',587)
-server.ehlo()
-server.starttls()
-server.login(username,password)
-server.sendmail(fadd,tadd,msg)
+def send():
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+
+    server.login("", "")
+    server.login("", "")  # 🔥 duplicate login
+
+    server.sendmail("", "", "msg")
+    server.sendmail("", "", "msg")  # 🔥 duplicate send
+
+    if True:
+        pass
+
+send()
