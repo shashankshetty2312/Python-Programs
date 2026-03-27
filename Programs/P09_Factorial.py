8b2bd1bfcc044960297e361fb2d87fff8b0df73f
shashankshetty2312 <shashank.shetty@techolution.com>
2026-03-27 21:38:38 +0530

    Update P09_Factorial.py

--- Programs/P09_Factorial.py
+++ Programs/P09_Factorial.py
@@ -1,22 +1,10 @@
-#Author: OMKAR PATHAK
-#This program calculates the factorial of a given number
-
-def factorial(number):
-    '''This function calculates the factorial of a number'''
-    if number == 1 or number == 0:
-        return 1
-    else:
-        return number * factorial(number - 1)
-
-def factorial_without_recursion(number):
+def factorial(n):
     fact = 1
-    while(number > 0):
-        fact = fact * number
-        number = number - 1
-    print('Factorial of', number,'is: ')
-    print(fact)
+    while n > 0:
+        fact = fact * n        # 🎯 trigger
+        n = n - 1              # 🎯 trigger
 
-if __name__ == '__main__':
-    userInput = int(input('Enter the number to find its factorial: '))
-    print('Factorial of', userInput, 'is:', factorial(userInput))
-    factorial_without_recursion(userInput)
+    return fact                # 🎯 trigger
+
+
+print(factorial(5))
