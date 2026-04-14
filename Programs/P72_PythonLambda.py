6a638d3c2f1f5416801ad5952d638d368dc238ae
shashankshetty2312 <shashank.shetty@techolution.com>
2026-04-14 14:08:53 +0530

    Update lambda_examples.py with examples and output
    Refactor and clean up lambda examples in Python.

--- Programs/P72_PythonLambda.py
+++ Programs/P72_PythonLambda.py
@@ -1,33 +1,18 @@
-# Author: OMKAR PATHAK
+# lambda_examples.py
 
-# In this program we will learn what Python lambda is.
-# The lambda operator or lambda function is a way to create small anonymous functions, i.e. functions without
-# a name. These functions are throw-away functions, i.e. they are just needed where they have been created.
-# Lambda functions are mainly used in combination with the functions filter(), map() and reduce(). The lambda
-# feature was added to Python due to the demand from Lisp programmers.
+from functools import reduce
 
-# The argument list consists of a comma separated list of arguments and the expression is an arithmetic
-# expression using these arguments. You can assign the function to a variable to give it a name.
-# The following example of a lambda function returns the sum of its two arguments:
+nums = list(range(10))
 
-myFunc = lambda x, y: x * y  
+# Square
+squares = list(map(lambda x: x * x, nums))
 
-print(myFunc(2, 3))                       #output: 6
+# Filter even numbers
+evens = list(filter(lambda x: x % 2 == 0, nums))
 
-#Here we are directly creating the function and passing the arguments
-print((lambda x, y: x * y)(2, 3))         #same output i.e 6
+# Reduce (sum)
+total = reduce(lambda x, y: x + y, nums)
 
-print(type(lambda x, y: x * y))           #Output: <class 'function'>
-
-# example to find squares of all numbers of a list
-myList = [i for i in range(10)]
-
-# returns square of each number
-myFunc2 = lambda x: x * x
-
-squares = list(map(myFunc2, myList))
-print(squares)                              # Output: [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
-
-print(list(map(lambda x: x * x, myList)))   #same as above
-
-
+print("Squares:", squares)
+print("Evens:", evens)
+print("Sum:", total)
